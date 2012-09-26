2bd85245d62f544682874a0816c5700915a37b59
James Yonan <james@openvpn.net>
2012-09-26 07:20:42 +0000

    LZO-Asym optimizations for ARM.

--- openvpn/compress/lzoasym_impl.hpp
+++ openvpn/compress/lzoasym_impl.hpp
@@ -8,19 +8,28 @@
 #ifndef OPENVPN_COMPRESS_LZOASYM_IMPL_H
 #define OPENVPN_COMPRESS_LZOASYM_IMPL_H
 
-#include <cstring> // for std::memcpy
-
 #include <boost/cstdint.hpp> // for boost::uint32_t, etc.
 
-// Implementation of asymmetrical LZO compression (only uncompress, don'z compress)
+// Implementation of asymmetrical LZO compression (only uncompress, don't compress)
 
-#define LZOASYM_CHECK_INPUT_OVERRUN(x) if (size_t(input_ptr_end - input_ptr) < size_t(x)) goto input_overrun
-#define LZOASYM_CHECK_OUTPUT_OVERRUN(x) if (size_t(output_ptr_end - output_ptr) < size_t(x)) goto output_overrun
-#define LZOASYM_CHECK_LOOKBEHIND_OVERRUN(match_ptr) if (match_ptr < output || match_ptr >= output_ptr) goto lookbehind_overrun
-#define LZOASYM_ASSERT(cond) if (!(cond)) goto assert_fail
+// Branch prediction hints (these make a difference on ARM)
+#if defined(__GNUC__)
+# define LZOASYM_LIKELY(x)       __builtin_expect((x),1)
+# define LZOASYM_UNLIKELY(x)     __builtin_expect((x),0)
+#else
+# define LZOASYM_LIKELY(x)      (x)
+# define LZOASYM_UNLIKELY(x)    (x)
+#endif
+
+// Failure modes
+#define LZOASYM_CHECK_INPUT_OVERRUN(x) if (LZOASYM_UNLIKELY(size_t(input_ptr_end - input_ptr) < size_t(x))) goto input_overrun
+#define LZOASYM_CHECK_OUTPUT_OVERRUN(x) if (LZOASYM_UNLIKELY(size_t(output_ptr_end - output_ptr) < size_t(x))) goto output_overrun
+#define LZOASYM_CHECK_LOOKBEHIND_OVERRUN(match_ptr) if (LZOASYM_UNLIKELY(match_ptr < output) || LZOASYM_UNLIKELY(match_ptr >= output_ptr)) goto lookbehind_overrun
+#define LZOASYM_ASSERT(cond) if (LZOASYM_UNLIKELY(!(cond))) goto assert_fail
 
 namespace openvpn {
   namespace lzo_asym_impl {
+    // Return status values
     enum {
       LZOASYM_E_OK=0,
       LZOASYM_E_EOF_NOT_FOUND=-1,
@@ -31,10 +40,13 @@ namespace openvpn {
       LZOASYM_E_ASSERT_FAILED=-6,
     };
 
+    // Internal constants
     enum {
       LZOASYM_EOF_CODE=1,
       LZOASYM_M2_MAX_OFFSET=0x0800,
     };
+
+    // Polymorphic get/set/copy
 
     template <typename T>
     inline T get_mem(const void *p)
@@ -64,12 +76,15 @@ namespace openvpn {
       return ((size_t(a) | size_t(b)) & 3) == 0;
     }
 
+
+    // take the number of objects difference between two pointers
     template <typename T>
     inline size_t ptr_diff(const T* a, const T* b)
     {
       return a - b;
     }
 
+    // read uint16_t from memory
     inline size_t get_u16(const unsigned char *p)
     {
       // NOTE: assumes little-endian and unaligned 16-bit access is okay.
@@ -77,6 +92,7 @@ namespace openvpn {
       return get_mem<boost::uint16_t>(p);
     }
 
+    // copy 64 bits
     inline void copy_64(unsigned char *dest, const unsigned char *src)
     {
       // NOTE: assumes that 64-bit machines can do 64-bit unaligned access, and
@@ -92,10 +108,11 @@ namespace openvpn {
 	}
     }
 
-    // NOTE: we might write up to ten extra bytes after the end of the copy
+    // Fast version of incremental copy.
+    // NOTE: we might write up to ten extra bytes after the end of the copy.
     inline void incremental_copy_fast(unsigned char *dest, const unsigned char *src, int len)
     {
-      while (dest - src < 8)
+      while (LZOASYM_UNLIKELY(dest - src < 8))
 	{
 	  copy_64(dest, src);
 	  len -= dest - src;
@@ -110,6 +127,7 @@ namespace openvpn {
 	}
     }
 
+    // Slow version of incremental copy
     inline void incremental_copy(unsigned char *dest, const unsigned char *src, int len)
     {
       do {
@@ -117,7 +135,8 @@ namespace openvpn {
       } while (--len);
     }
 
-    inline void memcpy_fast(unsigned char *dest, const unsigned char *src, int len)
+    // Faster version of memcpy
+    inline void copy_fast(unsigned char *dest, const unsigned char *src, int len)
     {
       while (len >= 8)
 	{
@@ -140,7 +159,7 @@ namespace openvpn {
 	case 2:
 	  *dest++ = *src++;
 	case 1:
-	  *dest++ = *src++;
+	  *dest = *src;
 	}
     }
 
@@ -149,9 +168,9 @@ namespace openvpn {
 			      unsigned char *output,
 			      size_t *output_length)
     {
-      unsigned char *output_ptr;
-      const unsigned char *input_ptr;
       size_t z;
+      const unsigned char *input_ptr;
+      unsigned char *output_ptr;
       const unsigned char *match_ptr;
       const unsigned char *const input_ptr_end = input + input_length;
       unsigned char *const output_ptr_end = output + *output_length;
@@ -161,8 +180,161 @@ namespace openvpn {
       input_ptr = input;
       output_ptr = output;
 
-      if (*input_ptr > 17)
+      if (LZOASYM_LIKELY(*input_ptr <= 17))
 	{
+	  while (LZOASYM_LIKELY(input_ptr < input_ptr_end) && LZOASYM_LIKELY(output_ptr <= output_ptr_end))
+	    {
+	      z = *input_ptr++;
+	      if (z < 16)
+		{
+		  // a literal run
+		  if (LZOASYM_UNLIKELY(z == 0))
+		    {
+		      LZOASYM_CHECK_INPUT_OVERRUN(1);
+		      while (LZOASYM_UNLIKELY(*input_ptr == 0))
+			{
+			  z += 255;
+			  input_ptr++;
+			  LZOASYM_CHECK_INPUT_OVERRUN(1);
+			}
+		      z += 15 + *input_ptr++;
+		    }
+
+		  // copy literal data
+		  {
+		    LZOASYM_ASSERT(z > 0);
+		    const size_t len = z + 3;
+		    LZOASYM_CHECK_OUTPUT_OVERRUN(len);
+		    LZOASYM_CHECK_INPUT_OVERRUN(len+1);
+		    copy_fast(output_ptr, input_ptr, len);
+		    input_ptr += len;
+		    output_ptr += len;
+		  }
+
+		initial_literal_run:
+		  z = *input_ptr++;
+		  if (LZOASYM_UNLIKELY(z < 16))
+		    {
+		      match_ptr = output_ptr - (1 + LZOASYM_M2_MAX_OFFSET);
+		      match_ptr -= z >> 2;
+		      match_ptr -= *input_ptr++ << 2;
+
+		      LZOASYM_CHECK_LOOKBEHIND_OVERRUN(match_ptr);
+		      LZOASYM_CHECK_OUTPUT_OVERRUN(3);
+		      *output_ptr++ = *match_ptr++;
+		      *output_ptr++ = *match_ptr++;
+		      *output_ptr++ = *match_ptr;
+		      goto match_done;
+		    }
+		}
+
+	      // found a match (M2, M3, M4, or M1)
+	      do {
+		if (LZOASYM_LIKELY(z >= 64))           // LZO "M2" match (most likely)
+		  {
+		    match_ptr = output_ptr - 1;
+		    match_ptr -= (z >> 2) & 7;
+		    match_ptr -= *input_ptr++ << 3;
+		    z = (z >> 5) - 1;
+		  }
+		else if (LZOASYM_LIKELY(z >= 32))      // LZO "M3" match
+		  {
+		    z &= 31;
+		    if (LZOASYM_UNLIKELY(z == 0))
+		      {
+			LZOASYM_CHECK_INPUT_OVERRUN(1);
+			while (LZOASYM_UNLIKELY(*input_ptr == 0))
+			  {
+			    z += 255;
+			    input_ptr++;
+			    LZOASYM_CHECK_INPUT_OVERRUN(1);
+			  }
+			z += 31 + *input_ptr++;
+		      }
+
+		    match_ptr = output_ptr - 1;
+		    match_ptr -= get_u16(input_ptr) >> 2;
+		    input_ptr += 2;
+		  }
+		else if (LZOASYM_LIKELY(z >= 16))      // LZO "M4" match
+		  {
+		    match_ptr = output_ptr;
+		    match_ptr -= (z & 8) << 11;
+		    z &= 7;
+		    if (LZOASYM_UNLIKELY(z == 0))
+		      {
+			LZOASYM_CHECK_INPUT_OVERRUN(1);
+			while (LZOASYM_UNLIKELY(*input_ptr == 0))
+			  {
+			    z += 255;
+			    input_ptr++;
+			    LZOASYM_CHECK_INPUT_OVERRUN(1);
+			  }
+			z += 7 + *input_ptr++;
+		      }
+
+		    match_ptr -= get_u16(input_ptr) >> 2;
+		    input_ptr += 2;
+		    if (LZOASYM_UNLIKELY(match_ptr == output_ptr))
+		      goto success;
+		    match_ptr -= 0x4000;
+		  }
+		else                                   // LZO "M1" match (least likely)
+		  {
+		    match_ptr = output_ptr - 1;
+		    match_ptr -= z >> 2;
+		    match_ptr -= *input_ptr++ << 2;
+
+		    LZOASYM_CHECK_LOOKBEHIND_OVERRUN(match_ptr);
+		    LZOASYM_CHECK_OUTPUT_OVERRUN(2);
+		    *output_ptr++ = *match_ptr++;
+		    *output_ptr++ = *match_ptr;
+		    goto match_done;
+		  }
+
+		// copy the match we found above
+		{
+		  LZOASYM_CHECK_LOOKBEHIND_OVERRUN(match_ptr);
+		  LZOASYM_ASSERT(z > 0);
+		  LZOASYM_CHECK_OUTPUT_OVERRUN(z+3-1);
+
+		  const size_t len = z + 2;
+		  // Should we use optimized incremental copy?
+		  // incremental_copy_fast might copy 10 more bytes than needed, so
+		  // don't use it unless we have enough trailing space in buffer.
+		  if (LZOASYM_LIKELY(size_t(output_ptr_end - output_ptr) >= len + 10))
+		    incremental_copy_fast(output_ptr, match_ptr, len);
+		  else
+		    incremental_copy(output_ptr, match_ptr, len);
+		  match_ptr += len;
+		  output_ptr += len;
+		}
+
+	      match_done:
+		z = input_ptr[-2] & 3;
+		if (LZOASYM_LIKELY(z == 0))
+		  break;
+
+	      match_next:
+		// copy literal data
+		LZOASYM_ASSERT(z > 0);
+		LZOASYM_ASSERT(z < 4);
+		LZOASYM_CHECK_OUTPUT_OVERRUN(z);
+		LZOASYM_CHECK_INPUT_OVERRUN(z+1);
+		*output_ptr++ = *input_ptr++;
+		if (LZOASYM_LIKELY(z > 1))
+		  {
+		    *output_ptr++ = *input_ptr++;
+		    if (z > 2)
+		      *output_ptr++ = *input_ptr++;
+		  }
+		z = *input_ptr++;
+	      } while (LZOASYM_LIKELY(input_ptr < input_ptr_end) && LZOASYM_LIKELY(output_ptr <= output_ptr_end));
+	    }
+	}
+      else
+	{
+	  // input began with a match or a literal run (rare)
 	  z = *input_ptr++ - 17;
 	  if (z < 4)
 	    goto match_next;
@@ -172,166 +344,17 @@ namespace openvpn {
 	  do {
 	    *output_ptr++ = *input_ptr++;
 	  } while (--z > 0);
-	  goto first_literal_run;
+	  goto initial_literal_run;
 	}
 
-      while ((input_ptr < input_ptr_end) && (output_ptr <= output_ptr_end))
-	{
-	  z = *input_ptr++;
-	  if (z >= 16)
-	    goto match_found;
-
-	  // a literal run
-	  if (z == 0)
-	    {
-	      LZOASYM_CHECK_INPUT_OVERRUN(1);
-	      while (*input_ptr == 0)
-		{
-		  z += 255;
-		  input_ptr++;
-		  LZOASYM_CHECK_INPUT_OVERRUN(1);
-		}
-	      z += 15 + *input_ptr++;
-	    }
-
-	  // copy literals
-	  {
-	    LZOASYM_ASSERT(z > 0);
-	    const size_t len = z + 3;
-	    LZOASYM_CHECK_OUTPUT_OVERRUN(len);
-	    LZOASYM_CHECK_INPUT_OVERRUN(len+1);
-	    memcpy_fast(output_ptr, input_ptr, len);
-	    input_ptr += len;
-	    output_ptr += len;
-	  }
-
-	first_literal_run:
-	  z = *input_ptr++;
-	  if (z >= 16)
-	    goto match_found;
-
-	  match_ptr = output_ptr - (1 + LZOASYM_M2_MAX_OFFSET);
-	  match_ptr -= z >> 2;
-	  match_ptr -= *input_ptr++ << 2;
-
-	  LZOASYM_CHECK_LOOKBEHIND_OVERRUN(match_ptr);
-	  LZOASYM_CHECK_OUTPUT_OVERRUN(3);
-	  *output_ptr++ = *match_ptr++;
-	  *output_ptr++ = *match_ptr++;
-	  *output_ptr++ = *match_ptr;
-	  goto match_done;
-
-	  // handle matches
-	  do {
-	  match_found:
-	    if (z >= 64)            // LZO "M2" match
-	      {
-		match_ptr = output_ptr - 1;
-		match_ptr -= (z >> 2) & 7;
-		match_ptr -= *input_ptr++ << 3;
-		z = (z >> 5) - 1;
-		goto copy_match;
-	      }
-	    else if (z >= 32)       // LZO "M3" match
-	      {
-		z &= 31;
-		if (z == 0)
-		  {
-		    LZOASYM_CHECK_INPUT_OVERRUN(1);
-		    while (*input_ptr == 0)
-		      {
-			z += 255;
-			input_ptr++;
-			LZOASYM_CHECK_INPUT_OVERRUN(1);
-		      }
-		    z += 31 + *input_ptr++;
-		  }
-
-		match_ptr = output_ptr - 1;
-		match_ptr -= get_u16(input_ptr) >> 2;
-		input_ptr += 2;
-	      }
-	    else if (z >= 16)       // LZO "M4" match
-	      {
-		match_ptr = output_ptr;
-		match_ptr -= (z & 8) << 11;
-		z &= 7;
-		if (z == 0)
-		  {
-		    LZOASYM_CHECK_INPUT_OVERRUN(1);
-		    while (*input_ptr == 0)
-		      {
-			z += 255;
-			input_ptr++;
-			LZOASYM_CHECK_INPUT_OVERRUN(1);
-		      }
-		    z += 7 + *input_ptr++;
-		  }
-
-		match_ptr -= get_u16(input_ptr) >> 2;
-		input_ptr += 2;
-		if (match_ptr == output_ptr)
-		  {
-		    LZOASYM_ASSERT(z == 1);
-		    *output_length = ptr_diff(output_ptr, output);
-		    return (input_ptr == input_ptr_end ? LZOASYM_E_OK :
-			    (input_ptr < input_ptr_end  ? LZOASYM_E_INPUT_NOT_CONSUMED : LZOASYM_E_INPUT_OVERRUN));
-		  }
-		match_ptr -= 0x4000;
-	      }
-	    else                    // LZO "M1" match
-	      {
-		match_ptr = output_ptr - 1;
-		match_ptr -= z >> 2;
-		match_ptr -= *input_ptr++ << 2;
-
-		LZOASYM_CHECK_LOOKBEHIND_OVERRUN(match_ptr);
-		LZOASYM_CHECK_OUTPUT_OVERRUN(2);
-		*output_ptr++ = *match_ptr++;
-		*output_ptr++ = *match_ptr;
-		goto match_done;
-	      }
-
-	  copy_match:
-	    {
-	      LZOASYM_CHECK_LOOKBEHIND_OVERRUN(match_ptr);
-	      LZOASYM_ASSERT(z > 0);
-	      LZOASYM_CHECK_OUTPUT_OVERRUN(z+3-1);
-
-	      const size_t len = z + 2;
-	      if (size_t(output_ptr_end - output_ptr) >= len + 10) // incremental_copy_fast might copy 10 more bytes than needed
-		incremental_copy_fast(output_ptr, match_ptr, len);
-	      else
-		incremental_copy(output_ptr, match_ptr, len);
-	      match_ptr += len;
-	      output_ptr += len;
-	    }
-
-	  match_done:
-	    z = input_ptr[-2] & 3;
-	    if (z == 0)
-	      break;
-
-	  match_next:
-	    // copy literals
-	    LZOASYM_ASSERT(z > 0);
-	    LZOASYM_ASSERT(z < 4);
-	    LZOASYM_CHECK_OUTPUT_OVERRUN(z);
-	    LZOASYM_CHECK_INPUT_OVERRUN(z+1);
-	    *output_ptr++ = *input_ptr++;
-	    if (z > 1)
-	      {
-		*output_ptr++ = *input_ptr++;
-		if (z > 2)
-		  *output_ptr++ = *input_ptr++;
-	      }
-	    z = *input_ptr++;
-	  } while ((input_ptr < input_ptr_end) && (output_ptr <= output_ptr_end));
-	}
-
-      // no EOF code was found
       *output_length = ptr_diff(output_ptr, output);
       return LZOASYM_E_EOF_NOT_FOUND;
+
+    success:
+      LZOASYM_ASSERT(z == 1);
+      *output_length = ptr_diff(output_ptr, output);
+      return (input_ptr == input_ptr_end ? LZOASYM_E_OK :
+	      (input_ptr < input_ptr_end  ? LZOASYM_E_INPUT_NOT_CONSUMED : LZOASYM_E_INPUT_OVERRUN));
 
     input_overrun:
       *output_length = ptr_diff(output_ptr, output);
@@ -355,5 +378,7 @@ namespace openvpn {
 #undef LZOASYM_CHECK_OUTPUT_OVERRUN
 #undef LZOASYM_CHECK_LOOKBEHIND_OVERRUN
 #undef LZOASYM_ASSERT
+#undef LZOASYM_LIKELY
+#undef LZOASYM_UNLIKELY
 
 #endif
