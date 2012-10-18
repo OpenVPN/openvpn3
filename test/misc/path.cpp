ff5ec86852e02155b3b5f089ddb2cd028d7c0087
James Yonan <james@openvpn.net>
2012-10-18 12:24:14 +0000

    [test/misc] Android 1.1.1 build 9
    * Fix attempt for java.lang.NullPointerException in
      net.openvpn.openvpn.OpenVPNService.onStartCommand(OpenVPNService.java:838)
    
    * Allow non-unified profiles (i.e. profiles containing directives that
      reference other files) to be imported from SD card, as long
      as all referenced files are present in the same directory on the
      SD card as the profile.
    
    * Relaxed parsing of "remote" directive to allow the port and/or
      protocol parameters to be omitted.  The port defaults to 1194
      and the protocol to UDP.  Either defaults can be changed with
      the "port" or "proto" directive.
    
    * Fixed issue where profile parser was choking on files containing
      Windows-style line-endings.

---
+++ test/misc/path.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+//#define OPENVPN_PATH_SIMULATE_WINDOWS
+
+#include <openvpn/common/types.hpp>
+#include <openvpn/common/exception.hpp>
+#include <openvpn/common/path.hpp>
+
+using namespace openvpn;
+
+void basename(const std::string& path)
+{
+  const std::string res = path::basename(path);
+  std::cout << "basename('" << path << "') = '" << res << "'" << std::endl;
+}
+
+void dirname(const std::string& path)
+{
+  const std::string res = path::dirname(path);
+  std::cout << "dirname('" << path << "') = '" << res << "'" << std::endl;
+}
+
+void ext(const std::string& path)
+{
+  const std::string res = path::ext(path);
+  std::cout << "ext('" << path << "') = '" << res << "'" << std::endl;
+}
+
+void is_flat(const std::string& path)
+{
+  const bool res = path::is_flat(path);
+  std::cout << "is_flat('" << path << "') = " << res << std::endl;
+}
+
+void join(const std::string& p1, const std::string& p2)
+{
+  const std::string res = path::join(p1, p2);
+  std::cout << "join('" << p1 << "', '" << p2 << "') = '" << res << "'" << std::endl;
+}
+
+
+int main()
+{
+  try {
+    // basename
+    basename("");
+    basename("/");
+    basename("/foo");
+    basename("/foo/bar");
+    basename("foo/bar/boo");
+    basename("foo/bar/");
+    basename("foo\\bar\\boo");
+
+    // dirname
+    dirname("");
+    dirname("/");
+    dirname("/foo");
+    dirname("/foo/bar");
+    dirname("foo/bar/boo");
+    dirname("foo/bar/");
+    dirname("foo\\bar\\boo");
+
+    // is_flat
+    is_flat("");
+    is_flat("/");
+    is_flat("foo.bar");
+    is_flat("foo/bar");
+    is_flat("c:/foo");
+    is_flat("c:foo");
+    is_flat("z:\\foo");
+    is_flat(".");
+    is_flat("..");
+    is_flat("./foo");
+
+    // join
+    join("foo", "bar");
+    join("foo", "");
+    join("", "foo/bar");
+    join("", "bar");
+    join("foo", "/bar");
+    join("/", "bar");
+
+    // ext
+    ext("");
+    ext("foo");
+    ext("foo.bar");
+    ext("foo.bar.moo");
+    ext("foo.");
+    ext(".foo");
+  }
+  catch (const std::exception& e)
+    {
+      std::cerr << "Exception: " << e.what() << std::endl;
+      return 1;
+    }
+  return 0;
+}
