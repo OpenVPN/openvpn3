7e0490f647f5206a0ab921ddc0b81aa303b96343
James Yonan <james@openvpn.net>
2014-01-13 17:48:37 -0700

    Updated Android SDK, NDK, and build scripts:
    SDK: adt-bundle-mac-x86_64-20131030
    NDK: android-ndk-r9c

--- polarssl/android.cmake
+++ polarssl/android.cmake
@@ -3,9 +3,9 @@ SET(CMAKE_SYSTEM_NAME Linux)
 #this one not so much
 SET(CMAKE_SYSTEM_VERSION 1)
 
-# specify the cross compiler
-SET(CMAKE_C_COMPILER   arm-linux-androideabi-gcc)
-SET(CMAKE_CXX_COMPILER arm-linux-androideabi-g++)
+# specify the cross compiler (assumes that PATH already points to android toolchain)
+SET(CMAKE_C_COMPILER   gcc)
+SET(CMAKE_CXX_COMPILER g++)
 
 # where is the target environment 
 #SET(CMAKE_FIND_ROOT_PATH  /opt/eldk-2007-01-19/ppc_74xx /home/alex/eldk-ppc74xx-inst)
