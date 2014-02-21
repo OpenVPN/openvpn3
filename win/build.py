3c284610219782f0d7575e69875677ef94e17774
James Yonan <james@openvpn.net>
2014-02-20 17:27:55 -0700

    Windows build script changes:
    * Raised minimum deployment target to Vista (from XP).
    
    * Added shell32.lib and ole32.lib to link path to
      accommodate calls to SHGetKnownFolderPath.

--- win/build.py
+++ win/build.py
@@ -24,5 +24,7 @@ paths = {
 }
 
 # add to link dynamically: /DBOOST_ALL_DYN_LINK
+# add to support pre-Vista: /D_WIN32_WINNT=0x0501
+# add to support Vista and later: /D_WIN32_WINNT=0x0600
 
-vc_cmd(PARMS, r"cl /D_WIN32_WINNT=0x0501 /DNOMINMAX /D_CRT_SECURE_NO_WARNINGS /DBOOST_ALL_DYN_LINK /DUSE_OPENSSL /I %(openssl)s\include /I %(boost)s /I %(tap)s -DTAP_WIN_COMPONENT_ID=%(tap_component_id)s /I %(ovpn3)s /GL /EHsc /MD /W3 %(dbg_rel_flags)s /nologo %(srcfile)s /link /LIBPATH:%(boost)s\stage\lib /LIBPATH:%(openssl)s\lib libeay32.lib ssleay32.lib ws2_32.lib crypt32.lib iphlpapi.lib winmm.lib user32.lib gdi32.lib advapi32.lib wininet.lib" % paths, arch=os.environ.get("ARCH"))
+vc_cmd(PARMS, r"cl /D_WIN32_WINNT=0x0600 /DNOMINMAX /D_CRT_SECURE_NO_WARNINGS /DBOOST_ALL_DYN_LINK /DUSE_OPENSSL /I %(openssl)s\include /I %(boost)s /I %(tap)s -DTAP_WIN_COMPONENT_ID=%(tap_component_id)s /I %(ovpn3)s /GL /EHsc /MD /W3 %(dbg_rel_flags)s /nologo %(srcfile)s /link /LIBPATH:%(boost)s\stage\lib /LIBPATH:%(openssl)s\lib libeay32.lib ssleay32.lib ws2_32.lib crypt32.lib iphlpapi.lib winmm.lib user32.lib gdi32.lib advapi32.lib wininet.lib shell32.lib ole32.lib" % paths, arch=os.environ.get("ARCH"))
