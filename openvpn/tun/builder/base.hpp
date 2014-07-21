2c6d972ad609fe45e68bacd1870755afaa23f91f
James Yonan <james@openvpn.net>
2014-07-20 21:22:06 -0600

    Added AGPL copyright/licensing language.

--- openvpn/tun/builder/base.hpp
+++ openvpn/tun/builder/base.hpp
@@ -1,9 +1,23 @@
+//    OpenVPN -- An application to securely tunnel IP networks
+//               over a single port, with support for SSL/TLS-based
+//               session authentication and key exchange,
+//               packet encryption, packet authentication, and
+//               packet compression.
 //
-//  base.hpp
-//  OpenVPN
+//    Copyright (C) 2013-2014 OpenVPN Technologies, Inc.
 //
-//  Copyright (c) 2012 OpenVPN Technologies, Inc. All rights reserved.
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License Version 3
+//    as published by the Free Software Foundation.
 //
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program in the COPYING file.
+//    If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef OPENVPN_TUN_BUILDER_BASE_H
 #define OPENVPN_TUN_BUILDER_BASE_H
