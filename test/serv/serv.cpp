19ded1f0a3752cd11b41073f6ecd34efd3e34303
James Yonan <james@openvpn.net>
2014-08-19 01:13:42 -0600

    Server development initial checkpoint:
    1. UDP server
    2. Client instance objects
    
    No routing, authentication, or management.
    
    Server will negotiate a client then push AUTH_FAILED.

--- test/serv/serv.cpp
+++ test/serv/serv.cpp
@@ -3,9 +3,16 @@
 
 #include <boost/bind.hpp>
 
-#include <openvpn/log/logsimple.hpp>
+#define OPENVPN_LOG_SSL(x) OPENVPN_LOG(x)
 
-#include <openvpn/time/asiotimer.hpp> // fixme
+// debug settings (production setting in parentheses)
+
+#define OPENVPN_INSTRUMENTATION        // include debug instrumentation for classes (define)
+#define OPENVPN_DEBUG_UDPLINK 2        // debug level for UDP link object (2)
+#define OPENVPN_DEBUG_PROTO 2          // increases low-level protocol verbosity (1)
+#define OPENVPN_DEBUG_SERVPROTO        // shows packets in/out (comment out)
+
+#include <openvpn/log/logsimple.hpp>
 
 #include <openvpn/common/types.hpp>
 #include <openvpn/common/exception.hpp>
@@ -15,10 +22,35 @@
 #include <openvpn/common/file.hpp>
 #include <openvpn/common/options.hpp>
 #include <openvpn/common/asiosignal.hpp>
+#include <openvpn/common/signal.hpp>
 #include <openvpn/common/asiodispatch.hpp>
 #include <openvpn/init/initprocess.hpp>
+#include <openvpn/frame/frame_init.hpp>
+#include <openvpn/log/sessionstats.hpp>
+#include <openvpn/ssl/sslchoose.hpp>
+
+#include <openvpn/transport/server/udpserv.hpp>
+#include <openvpn/server/servproto.hpp>
 
 using namespace openvpn;
+
+class MySessionStats : public SessionStats
+{
+public:
+  typedef boost::intrusive_ptr<MySessionStats> Ptr;
+
+  virtual void error(const size_t err_type, const std::string* text=NULL)
+  {
+    if (text)
+      OPENVPN_LOG("ERROR: " << Error::name(err_type) << ": " << *text);
+    else
+      OPENVPN_LOG("ERROR: " << Error::name(err_type));
+  }
+};
+
+struct MyTransportParent : public TransportServerParent
+{
+};
 
 class ServerThread : public RC<thread_unsafe_refcount>
 {
@@ -27,30 +59,53 @@ public:
 
   struct Config
   {
+    TransportServerFactory::Ptr transport_factory;
   };
 
   ServerThread(boost::asio::io_service& io_service_arg,
 	       const Config& config_arg)
     : io_service(io_service_arg),
-      config(config_arg)
+      config(config_arg),
+      halt(false)
   {
+    transport_server = config.transport_factory->new_server_obj(io_service, transport_parent);
   }
 
   void start()
   {
+    if (!halt)
+      {
+	transport_server->start();
+	OPENVPN_LOG("Local endpoint: " << transport_server->local_endpoint_info());
+      }
   }
 
   void stop()
   {
+    if (!halt)
+      {
+	halt = true;
+	OPENVPN_LOG("ServerThread::stop called"); // fixme
+	transport_server->stop();
+      }
   }
 
   void thread_safe_stop()
   {
+    if (!halt)
+      {
+	OPENVPN_LOG("ServerThread::thread_safe_stop called"); // fixme
+	io_service.post(asio_dispatch_post(&ServerThread::stop, this));
+      }
   }
 
 private:
   boost::asio::io_service& io_service;
   Config config;
+  bool halt;
+
+  MyTransportParent transport_parent;
+  TransportServer::Ptr transport_server;
 };
 
 class MyRunContext : public RC<thread_safe_refcount>
@@ -62,7 +117,6 @@ public:
     : io_service(1),
       signals(new ASIOSignals(io_service)),
       serv(serv_arg),
-      debug_timer(io_service), // fixme
       halt(false)
   {
     signals->register_signals(asio_dispatch_signal(&MyRunContext::signal, this));
@@ -72,7 +126,6 @@ public:
   {
     if (!halt)
       {
-	schedule_debug_timer(); // fixme
 	io_service.run();
       }
   }
@@ -82,8 +135,10 @@ public:
     if (!halt)
       {
 	halt = true;
-	OPENVPN_LOG("CANCEL"); // fixme
+	OPENVPN_LOG("MyRunContext::cancel called"); // fixme
 	io_service.post(asio_dispatch_post(&ASIOSignals::cancel, signals.get()));
+	if (serv)
+	  serv->thread_safe_stop();
       }
   }
 
@@ -93,77 +148,128 @@ private:
     if (!error && !halt)
       {
 	OPENVPN_LOG("ASIO SIGNAL " << signal_number); // fixme
-	if (serv)
-	  serv->thread_safe_stop();
-	debug_timer.cancel(); // fixme
-      }
-  }
-
-  void debug_timeout_handler(const boost::system::error_code& e) // called by Asio
-  {
-    if (!e)
-      {
-	OPENVPN_LOG("DBG TIMER FIRED"); // fixme
 	cancel();
       }
-  }
-
-  void schedule_debug_timer() // fixme
-  {
-    OPENVPN_LOG("SCHED DBG TIMER"); // fixme
-    debug_timer.expires_at(Time::now() + Time::Duration::seconds(5)); // fixme
-    debug_timer.async_wait(asio_dispatch_timer(&MyRunContext::debug_timeout_handler, this)); // fixme
   }
 
   boost::asio::io_service io_service;
   ASIOSignals::Ptr signals;
   ServerThread::Ptr& serv;
 
-  AsioTimer debug_timer; // fixme
   bool halt;
 };
 
 void work(const char *config_fn, ServerThread::Ptr& serv)
 {
+  typedef ServerProto<SSLLib::RandomAPI, SSLLib::CryptoAPI, SSLLib::SSLAPI>::Factory ServerProtoFactory;
+
+  // set global PolarSSL debug level
+#if defined(USE_POLARSSL) && defined(OPENVPN_SSL_DEBUG)
+  debug_set_threshold(OPENVPN_SSL_DEBUG);
+#endif
+
+  // current time
+  Time now;
+
   // parse options from server config file
   OptionList opt = OptionList::parse_from_config_static(read_text(config_fn), NULL);
-
-  // set up server config
-  ServerThread::Config server_config;
 
   // initialize the Asio io_service object (for worker thread)
   boost::asio::io_service io_service(1); // concurrency hint=1
 
+  // initialize RNG/PRNG
+  SSLLib::RandomAPI::Ptr rng(new SSLLib::RandomAPI());
+  PRNG<SSLLib::RandomAPI, SSLLib::CryptoAPI>::Ptr prng(new PRNG<SSLLib::RandomAPI, SSLLib::CryptoAPI>("SHA1", rng, 16));
+
+  // initialize frame
+  Frame::Ptr frame = frame_init();
+
+  // initialize statistics
+  SessionStats::Ptr stats = new MySessionStats();
+
+  // initialize SSL
+  SSLLib::SSLAPI::Config ssl;
+  ssl.frame = frame;
+  ssl.flags = SSLConst::LOG_VERIFY_STATUS;
+#ifdef OPENVPN_SSL_DEBUG
+  ssl.ssl_debug_level = OPENVPN_SSL_DEBUG;
+#endif
+#if defined(USE_POLARSSL) || defined(USE_POLARSSL_APPLE_HYBRID)
+  ssl.rng = rng;
+#endif
+  ssl.load(opt);
+  if (!ssl.mode.is_server())
+    throw option_error("only server configuration supported");
+
+  // initialize OpenVPN protocol config extras
+  ProtoContextOptions pco;
+
+  // initialize main OpenVPN protocol config
+  ServerProtoFactory::ProtoConfig::Ptr pcfg(new ServerProtoFactory::ProtoConfig());
+  pcfg->load(opt, pco, -1);
+  pcfg->ssl_ctx.reset(new SSLLib::SSLAPI(ssl));
+  pcfg->set_protocol(Protocol(Protocol::UDP));
+  pcfg->frame = frame;
+  pcfg->now = &now;
+  pcfg->rng = rng;
+  pcfg->prng = prng;
+
+  // initialize factory to create client instance objects
+  ServerProtoFactory::Ptr spf(new ServerProtoFactory(io_service, *pcfg));
+  spf->proto_context_config = pcfg;
+  spf->stats = stats;
+
+  // initialize transport layer factory
+  UDPTransport::ServerConfig::Ptr usc(UDPTransport::ServerConfig::new_obj());
+  usc->local_ip = opt.get("local", 1, 64);
+  usc->local_port = opt.get("port", 1, 16);
+  usc->frame = frame;
+  usc->stats = stats;
+  usc->client_instance_factory = spf;
+
+  // set up server config
+  ServerThread::Config server_config;
+  server_config.transport_factory = usc;
+
   // instantiate top-level server session
   serv.reset(new ServerThread(io_service, server_config));
 
-  // start VPN
+  // listen for VPN clients
   serv->start(); // queue parallel async reads
 
   // run i/o reactor
   try {
     io_service.run();
+    OPENVPN_LOG("Worker thread event loop terminated normally"); // fixme
   }
-  catch (const std::exception&)
+  catch (...)
     {
+      OPENVPN_LOG("Worker thread event loop hit exception"); // fixme
       serv->stop();      // on exception, stop server,
       io_service.poll(); //   execute completion handlers,
-      throw;             //   and rethrow exception
+      throw;
     }
 }
 
 void worker_thread(const char *config_fn, ServerThread::Ptr& serv, MyRunContext::Ptr run_context)
 {
-  boost::asio::detail::signal_blocker signal_blocker; // signals should be handled by parent thread
+  SignalBlocker signal_blocker( // these signals should be handled by parent thread
+			       Signal::F_SIGINT|
+			       Signal::F_SIGTERM|
+			       Signal::F_SIGHUP|
+			       Signal::F_SIGUSR1|
+			       Signal::F_SIGUSR2);
   try {
-    std::cout << "WORKER THREAD STARTING" << std::endl;
+    OPENVPN_LOG("WORKER THREAD STARTING");
     work(config_fn, serv);
+    OPENVPN_LOG("WORKER THREAD finishing normally"); // fixme
   }
   catch (const std::exception& e)
     {
-      std::cerr << "Worker thread exception: " << e.what() << std::endl;
+      OPENVPN_LOG("Worker thread exception: " << e.what());
     }
-  //run_context->cancel(); // fixme
+  run_context->cancel();
+  OPENVPN_LOG("WORKER THREAD exiting"); // fixme
 }
 
 int main(int argc, char* argv[])
@@ -186,17 +292,18 @@ int main(int argc, char* argv[])
 	// wait for worker to exit
 	run_context->run();
 	thread->join();
+	OPENVPN_LOG("worker thread exited, join returned"); // fixme
       }
     else
       {
-	std::cerr << "OpenVPN Server (C++ core)" << std::endl;
-	std::cerr << "usage: " << argv[0] << " <config-file>" << std::endl;
+	OPENVPN_LOG("OpenVPN Server (C++ core)");
+        OPENVPN_LOG("usage: " << argv[0] << " <config-file>");
 	ret = 2;
       }
   }
   catch (const std::exception& e)
     {
-      std::cerr << "Main thread exception: " << e.what() << std::endl;
+      OPENVPN_LOG("Main thread exception: " << e.what());
       ret = 1;
     }
 
