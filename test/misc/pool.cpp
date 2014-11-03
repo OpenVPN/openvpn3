1d8fcb614a32f02e5ae5135dab0ff3309551043c
James Yonan <james@openvpn.net>
2014-11-03 08:35:59 -0700

    [test/misc] Added/modified tests for Pool and VPNServerNetblock.

--- test/misc/pool.cpp
+++ test/misc/pool.cpp
@@ -8,28 +8,27 @@ using namespace openvpn;
 int main()
 {
   try {
-    typedef IP::Addr Addr;
-    IP::Pool<Addr> pool;
-    pool.add_range(IP::Range<Addr>(Addr::from_string("1.2.3.4"), 16));
-    pool.add_range(IP::Range<Addr>(Addr::from_string("Fe80::23a1:b152"), 4));
-    pool.add_addr(Addr::from_string("10.10.1.1"));
+    IP::Pool pool;
+    pool.add_range(IP::Range(IP::Addr::from_string("1.2.3.4"), 16));
+    pool.add_range(IP::Range(IP::Addr::from_string("Fe80::23a1:b152"), 4));
+    pool.add_addr(IP::Addr::from_string("10.10.1.1"));
     std::cout << "LF " << pool.load_factor() << std::endl;
-    const bool b = pool.acquire_specific_addr(Addr::from_string("1.2.3.10"));
+    const bool b = pool.acquire_specific_addr(IP::Addr::from_string("1.2.3.10"));
     std::cout << "GET 10: " << b << std::endl;
     for (int i = 0; ; ++i)
       {
-	Addr addr;
+	IP::Addr addr;
 	if (i == 7)
 	  {
 	    std::cout << "REL 7" << std::endl;
-	    pool.release_addr(Addr::from_string("1.2.3.7"));
+	    pool.release_addr(IP::Addr::from_string("1.2.3.7"));
 	  }
 	else if (i == 11)
 	  {
 	    std::cout << "REL 3,4,5" << std::endl;
-	    pool.release_addr(Addr::from_string("1.2.3.3"));
-	    pool.release_addr(Addr::from_string("1.2.3.4"));
-	    pool.release_addr(Addr::from_string("1.2.3.5"));
+	    pool.release_addr(IP::Addr::from_string("1.2.3.3"));
+	    pool.release_addr(IP::Addr::from_string("1.2.3.4"));
+	    pool.release_addr(IP::Addr::from_string("1.2.3.5"));
 	  }
 	else
 	  {
