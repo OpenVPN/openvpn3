95e11e917272d28398dc0a1057808e7980268ef5
James Yonan <james@openvpn.net>
2015-04-22 17:53:12 -0600

    Added openvpn/common/stat.hpp with file_exists() method.

---
+++ openvpn/common/stat.hpp
@@ -0,0 +1,40 @@
+//    OpenVPN -- An application to securely tunnel IP networks
+//               over a single port, with support for SSL/TLS-based
+//               session authentication and key exchange,
+//               packet encryption, packet authentication, and
+//               packet compression.
+//
+//    Copyright (C) 2012-2015 OpenVPN Technologies, Inc.
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License Version 3
+//    as published by the Free Software Foundation.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program in the COPYING file.
+//    If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef OPENVPN_COMMON_STAT_H
+#define OPENVPN_COMMON_STAT_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+namespace openvpn {
+
+  // Return true if file exists
+  inline bool file_exists(const std::string& filename)
+  {
+    struct stat buffer;   
+    return stat(filename.c_str(), &buffer) == 0; 
+  }
+
+}
+
+#endif
