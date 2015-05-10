4f58b7f76b36759edb6b5277cca4d7b71812ec54
James Yonan <james@openvpn.net>
2015-05-10 17:48:53 -0600

    [test/misc] Added std::atomic and RC tests.

---
+++ test/misc/rc.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include <openvpn/log/logsimple.hpp>
+#include <openvpn/common/types.hpp>
+#include <openvpn/common/exception.hpp>
+
+#include <openvpn/common/rc.hpp>
+
+using namespace openvpn;
+
+class Test : public RC<thread_safe_refcount>
+{
+public:
+  typedef boost::intrusive_ptr<Test> Ptr;
+
+  Test()
+  {
+    OPENVPN_LOG("Test()");
+  }
+
+  ~Test()
+  {
+    OPENVPN_LOG("~Test()");
+  }
+};
+
+int main(int /*argc*/, char* /*argv*/[])
+{
+  try {
+    Test::Ptr t1 = new Test();
+    Test::Ptr t2(t1);
+    Test::Ptr t3(t2);
+  }
+  catch (const std::exception& e)
+    {
+      std::cerr << "Exception: " << e.what() << std::endl;
+      return 1;
+    }
+  return 0;
+}
