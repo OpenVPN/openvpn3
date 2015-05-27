16bb7df1d2f075564645dd85aa936bd32d006bef
James Yonan <james@openvpn.net>
2015-05-26 22:45:24 -0600

    Added support for weak pointers to rc.hpp.

--- openvpn/common/rc.hpp
+++ openvpn/common/rc.hpp
@@ -67,13 +67,59 @@
 
 namespace openvpn {
 
-  typedef olong thread_unsafe_refcount;
+  // The smart pointer
+  template<typename T>
+  using RCPtr = boost::intrusive_ptr<T>;
+
+  class thread_unsafe_refcount
+  {
+    thread_unsafe_refcount(const thread_unsafe_refcount&) = delete;
+    thread_unsafe_refcount& operator=(const thread_unsafe_refcount&) = delete;
+
+  public:
+    thread_unsafe_refcount() noexcept
+      : rc(olong(0))
+    {
+    }
+
+    void operator++()
+    {
+      ++rc;
+    }
+
+    olong operator--()
+    {
+      return --rc;
+    }
+
+    bool inc_if_nonzero()
+    {
+      if (rc)
+	{
+	  ++rc;
+	  return true;
+	}
+      else
+	return false;
+    }
+
+    olong use_count() const
+    {
+      return rc;
+    }
+
+  private:
+    olong rc;
+  };
 
   class thread_safe_refcount
   {
+    thread_safe_refcount(const thread_safe_refcount&) = delete;
+    thread_safe_refcount& operator=(const thread_safe_refcount&) = delete;
+
   public:
-    thread_safe_refcount(const olong value)
-      : rc(value)
+    thread_safe_refcount() noexcept
+      : rc(olong(0))
     {
     }
 
@@ -84,10 +130,31 @@ namespace openvpn {
 
     olong operator--()
     {
+      // http://www.boost.org/doc/libs/1_55_0/doc/html/atomic/usage_examples.html
       const olong ret = rc.fetch_sub(1, std::memory_order_release) - 1;
       if (ret == 0)
 	std::atomic_thread_fence(std::memory_order_acquire);
       return ret;
+    }
+
+    // If refcount is 0, do nothing and return false.
+    // If refcount != 0, increment it and return true.
+    bool inc_if_nonzero()
+    {
+      olong previous = rc.load(std::memory_order_relaxed);
+      while (true)
+	{
+	  if (!previous)
+	    break;
+	  if (rc.compare_exchange_weak(previous, previous + 1, std::memory_order_relaxed))
+	    break;
+	}
+      return previous > 0;
+    }
+
+    olong use_count() const
+    {
+      return rc.load(std::memory_order_relaxed);
     }
 
   private:
@@ -103,7 +170,7 @@ namespace openvpn {
     RC& operator=(const RC&) = delete;
 
   public:
-    RC() noexcept : refcount_(0) {}
+    RC() noexcept {}
     virtual ~RC() {}
   private:
     template <typename R> friend void intrusive_ptr_add_ref(R* p) noexcept;
@@ -116,14 +183,162 @@ namespace openvpn {
   class RCCopyable
   {
   public:
-    RCCopyable() noexcept : refcount_(0) {}
-    RCCopyable(const RCCopyable&) noexcept : refcount_(0) {}
+    RCCopyable() noexcept {}
+    RCCopyable(const RCCopyable&) noexcept {}
     RCCopyable& operator=(const RCCopyable&) noexcept { return *this; }
     virtual ~RCCopyable() {}
   private:
     template <typename R> friend void intrusive_ptr_add_ref(R* p) noexcept;
     template <typename R> friend void intrusive_ptr_release(R* p) noexcept;
     RCImpl refcount_;
+  };
+
+  // Like RC, but also allows weak pointers
+  template <typename RCImpl> // RCImpl = thread_safe_refcount or thread_unsafe_refcount
+  class RCWeak
+  {
+    RCWeak(const RCWeak&) = delete;
+    RCWeak& operator=(const RCWeak&) = delete;
+
+    template<typename T>
+    friend class RCWeakPtr;
+
+    // For weak-referenceable objects, we must detach the
+    // refcount from the object and place it in Controller.
+    struct Controller : public RC<RCImpl>
+    {
+      typedef RCPtr<Controller> Ptr;
+
+      Controller(RCWeak* parent_arg) noexcept
+	: parent(parent_arg)
+      {
+      }
+
+      olong use_count() const
+      {
+	return rc.use_count();
+      }
+
+      template <typename PTR>
+      PTR lock()
+      {
+	if (rc.inc_if_nonzero())
+	  return PTR(static_cast<typename PTR::element_type*>(parent), false);
+	else
+	  return PTR();
+      }
+
+      RCWeak *const parent; // dangles after rc == 0
+      RCImpl rc;
+    };
+
+    struct ControllerRef
+    {
+      ControllerRef(RCWeak* parent) noexcept
+        : controller(new Controller(parent))
+      {
+      }
+
+      void operator++()
+      {
+	++controller->rc;
+      }
+
+      olong operator--()
+      {
+	return --controller->rc;
+      }
+
+      typename Controller::Ptr controller;
+    };
+
+  public:
+    RCWeak() noexcept
+      : refcount_(this)
+    {
+    }
+
+    virtual ~RCWeak()
+    {
+    }
+
+  private:
+    template <typename R> friend void intrusive_ptr_add_ref(R* p) noexcept;
+    template <typename R> friend void intrusive_ptr_release(R* p) noexcept;
+    ControllerRef refcount_;
+  };
+
+  template <typename T>
+  class RCWeakPtr
+  {
+    typedef RCPtr<T> Strong;
+
+  public:
+    typedef T element_type;
+
+    RCWeakPtr() noexcept {}
+
+    RCWeakPtr(const Strong& p) noexcept
+    {
+      if (p)
+	controller = p->refcount_.controller;
+    }
+
+    RCWeakPtr(T* p) noexcept
+    {
+      if (p)
+	controller = p->refcount_.controller;
+    }
+
+    void reset(const Strong& p) noexcept
+    {
+      if (p)
+	controller = p->refcount_.controller;
+      else
+	controller.reset();
+    }
+
+    void reset(T* p) noexcept
+    {
+      if (p)
+	controller = p->refcount_.controller;
+      else
+	controller.reset();
+    }
+
+    void reset() noexcept
+    {
+      controller.reset();
+    }
+
+    void swap(RCWeakPtr& other) noexcept
+    {
+      controller.swap(other.controller);
+    }
+
+    olong use_count() const
+    {
+      if (controller)
+	return controller->use_count();
+      else
+	return 0;
+    }
+
+    bool expired() const
+    {
+      return use_count() == 0;
+    }
+
+    Strong lock() const
+    {
+      if (controller)
+	return controller->template lock<Strong>();
+      else
+	return Strong();
+    }
+
+  private:
+    typename T::Controller::Ptr controller;
   };
 
 #if !defined(OPENVPN_RC_USERDEF)
@@ -142,10 +357,6 @@ namespace openvpn {
   }
 
 #endif
-
-  // The smart pointer
-  template<typename T>
-  using RCPtr = boost::intrusive_ptr<T>;
 
 } // namespace openvpn
 
