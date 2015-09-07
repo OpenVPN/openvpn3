1871d5671d6e6bd9f10cc20b26522acb16b30529
James Yonan <james@openvpn.net>
2015-09-07 06:16:54 -0700

    Added ZLib::decompress_gzip() method.

--- openvpn/buffer/zlib.hpp
+++ openvpn/buffer/zlib.hpp
@@ -28,11 +28,22 @@
 
 #include <openvpn/common/exception.hpp>
 #include <openvpn/buffer/buffer.hpp>
-#include <openvpn/frame/frame.hpp>
+#include <openvpn/buffer/buflist.hpp>
 
 namespace openvpn {
   namespace ZLib {
     OPENVPN_EXCEPTION(zlib_error);
+
+    class ZStreamBase // used internally by compress_gzip/decompress_gzip
+    {
+    public:
+      z_stream s;
+    protected:
+      ZStreamBase()  { std::memset(&s, 0, sizeof(s)); }
+    private:
+      ZStreamBase(const ZStreamBase&) = delete;
+      ZStreamBase& operator=(const ZStreamBase&) = delete;
+    };
 
     inline BufferPtr compress_gzip(BufferPtr src,
 				   const size_t headroom,
@@ -41,16 +52,19 @@ namespace openvpn {
 				   const int window_bits=15,
 				   const int mem_level=8)
     {
-      const int GZIP_ENCODING = 16;
+      constexpr int GZIP_ENCODING = 16;
+
+      struct ZStream : public ZStreamBase {
+	~ZStream() { ::deflateEnd(&s); }
+      };
+
       if (src)
 	{
 	  int status;
-	  z_stream zs;
-
-	  std::memset(&zs, 0, sizeof(zs));
-	  zs.next_in = src->data();
-	  zs.avail_in = src->size();
-	  status = ::deflateInit2(&zs,
+	  ZStream zs;
+	  zs.s.next_in = src->data();
+	  zs.s.avail_in = src->size();
+	  status = ::deflateInit2(&zs.s,
 				  level,
 				  Z_DEFLATED,
 				  GZIP_ENCODING + window_bits,
@@ -58,21 +72,76 @@ namespace openvpn {
 				  Z_DEFAULT_STRATEGY);
 	  if (status != Z_OK)
 	    OPENVPN_THROW(zlib_error, "zlib deflateinit2 failed, error=" << status);
-	  const uLong outcap = ::deflateBound(&zs, src->size());
+	  const uLong outcap = ::deflateBound(&zs.s, src->size());
 	  BufferPtr b = new BufferAllocated(outcap + headroom + tailroom, 0);
 	  b->init_headroom(headroom);
-	  zs.next_out = b->data();
-	  zs.avail_out = outcap;
-	  status = ::deflate(&zs, Z_FINISH);
+	  zs.s.next_out = b->data();
+	  zs.s.avail_out = outcap;
+	  status = ::deflate(&zs.s, Z_FINISH);
 	  if (status != Z_STREAM_END)
 	    OPENVPN_THROW(zlib_error, "zlib deflate failed, error=" << status);
-	  b->set_size(zs.total_out);
-	  ::deflateEnd(&zs);
+	  b->set_size(zs.s.total_out);
+#ifdef OPENVPN_GZIP_DEBUG
+	  OPENVPN_LOG("*** COMPRESS " << src->size() << " -> " << b->size());
+#endif
 	  return b;
 	}
       else
 	return BufferPtr();
     }
+
+    inline BufferPtr decompress_gzip(BufferPtr src,
+				     const size_t headroom,
+				     const size_t tailroom,
+				     const size_t max_size,
+				     const size_t block_size=4096,
+				     const int window_bits=15)
+    {
+      constexpr int GZIP_ENCODING = 16;
+
+      struct ZStream : public ZStreamBase {
+	~ZStream() { ::inflateEnd(&s); }
+      };
+
+      if (src)
+	{
+	  int status;
+	  ZStream zs;
+	  zs.s.next_in = src->data();
+	  zs.s.avail_in = src->size();
+	  status = ::inflateInit2(&zs.s, GZIP_ENCODING + window_bits);
+	  if (status != Z_OK)
+	    OPENVPN_THROW(zlib_error, "zlib inflateinit2 failed, error=" << status);
+
+	  BufferList blist;
+	  size_t hr = headroom;
+	  size_t tr = tailroom;
+	  do {
+	    // use headroom/tailroom on first block to take advantage
+	    // of BufferList::join() optimization for one-block lists
+	    BufferPtr b = new BufferAllocated(block_size + hr + tr, 0);
+	    b->init_headroom(hr);
+	    const size_t avail = b->remaining(tr);
+	    zs.s.next_out = b->data();
+	    zs.s.avail_out = avail;
+	    status = ::inflate(&zs.s, Z_SYNC_FLUSH);
+	    if (status != Z_OK && status != Z_STREAM_END)
+	      OPENVPN_THROW(zlib_error, "zlib inflate failed, error=" << status);
+	    b->set_size(avail - zs.s.avail_out);
+	    blist.push_back(std::move(b));
+	    if (zs.s.total_out > max_size)
+	      OPENVPN_THROW(zlib_error, "zlib inflate max_size " << max_size << " exceeded");
+	    hr = tr = 0;
+	  } while (status == Z_OK);
+#ifdef OPENVPN_GZIP_DEBUG
+	  OPENVPN_LOG("*** DECOMPRESS " << src->size() << " -> " << blist.join_size());
+#endif
+	  return blist.join(headroom, tailroom);
+	}
+      else
+	return BufferPtr();
+    }
+
   }
 }
 
