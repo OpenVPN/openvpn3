40bc68bb7451f880156eb4525ed6d7812503793b
James Yonan <james@openvpn.net>
2015-09-08 05:55:08 -0700

    Added SockOpt::Creds::root_or_self_uid() method to test if local socket client is root or has our own UID.

--- openvpn/common/peercred.hpp
+++ openvpn/common/peercred.hpp
@@ -22,6 +22,7 @@
 #ifndef OPENVPN_COMMON_PEERCRED_H
 #define OPENVPN_COMMON_PEERCRED_H
 
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 
@@ -37,6 +38,11 @@ namespace openvpn {
 	  gid(gid_arg),
 	  pid(pid_arg)
       {
+      }
+
+      bool root_or_self_uid() const
+      {
+	return !uid || uid == ::getuid();
       }
 
       int uid;
