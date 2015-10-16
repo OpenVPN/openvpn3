f6ae9caec335cc690c794456297df6e551744bf2
James Yonan <james@openvpn.net>
2015-10-15 23:40:52 -0700

    In tlsver.hpp, break out parse_tls_version_min() core functionality into a new method that can be called with simple parameters and without an OptionList.

--- openvpn/ssl/tlsver.hpp
+++ openvpn/ssl/tlsver.hpp
@@ -56,21 +56,30 @@ namespace openvpn {
 	}
     }
 
-    inline Type parse_tls_version_min(const OptionList& opt, const Type max_version) {
+    inline Type parse_tls_version_min(const std::string& ver,
+				      const bool or_highest,
+				      const Type max_version)
+    {
+      if (ver == "1.0" && V1_0 <= max_version)
+	return V1_0;
+      else if (ver == "1.1" && V1_1 <= max_version)
+	return V1_1;
+      else if (ver == "1.2" && V1_2 <= max_version)
+	return V1_2;
+      else if (or_highest)
+	return max_version;
+      else
+	throw option_error("tls-version-min: unrecognized TLS version");
+    }
+
+    inline Type parse_tls_version_min(const OptionList& opt, const Type max_version)
+    {
       const Option* o = opt.get_ptr("tls-version-min");
       if (o)
 	{
-	  const std::string& ct = o->get_optional(1, 16);
-	  if (ct == "1.0" && V1_0 <= max_version)
-	    return V1_0;
-	  else if (ct == "1.1" && V1_1 <= max_version)
-	    return V1_1;
-	  else if (ct == "1.2" && V1_2 <= max_version)
-	    return V1_2;
-	  else if (o->get_optional(2, 16) == "or-highest")
-	    return max_version;
-	  else
-	    throw option_error("tls-version-min: unrecognized TLS version");
+	  const std::string& ver = o->get_optional(1, 16);
+	  const bool or_highest = (o->get_optional(2, 16) == "or-highest");
+	  return parse_tls_version_min(ver, or_highest, max_version);
 	}
       return UNDEF;
     }
