1fb59fb1eb7d706e25dfcbcb4f345b6855c19a5c
James Yonan <james@openvpn.net>
2016-03-02 00:57:18 -0700

    [test/misc] Added test for OptionList::parse_from_argv_static().

---
+++ test/misc/parseargv.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <openvpn/common/exception.hpp>
+#include <openvpn/common/string.hpp>
+#include <openvpn/common/options.hpp>
+
+using namespace openvpn;
+
+int main(int argc, char* argv[])
+{
+  try {
+    const OptionList opt = OptionList::parse_from_argv_static(string::from_argv(argc, argv, true));
+    std::cout << opt.render(Option::RENDER_NUMBER|Option::RENDER_BRACKET);
+  }
+  catch (const std::exception& e)
+    {
+      std::cerr << "Exception: " << e.what() << std::endl;
+      return 1;
+    }
+  return 0;
+}
