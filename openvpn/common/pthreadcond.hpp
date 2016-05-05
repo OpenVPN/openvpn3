096a8a09fbae6668e3eb14b2f07f9c7545248db7
James Yonan <james@openvpn.net>
2016-05-05 00:30:11 -0600

    PThreadBarrier : major refactor to use C++11 <condition_variable> header rather than pthreads directly.
    Added asynchronous stop support.

--- openvpn/common/pthreadcond.hpp
+++ openvpn/common/pthreadcond.hpp
@@ -22,157 +22,20 @@
 #ifndef OPENVPN_COMMON_PTHREADCOND_H
 #define OPENVPN_COMMON_PTHREADCOND_H
 
-#include <openvpn/common/platform.hpp>
-#include <openvpn/common/abort.hpp>
-#include <openvpn/common/format.hpp>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
 
-#include <pthread.h>
-#include <time.h>
-#include <errno.h>
+#include <openvpn/common/stop.hpp>
 
 namespace openvpn {
-
-  class PThreadCondBase
-  {
-  protected:
-    PThreadCondBase()
-      : cond(PTHREAD_COND_INITIALIZER),
-	mutex(PTHREAD_MUTEX_INITIALIZER)
-    {
-    }
-
-    void lock()
-    {
-      const int status = pthread_mutex_lock(&mutex);
-      if (status)
-	error("pthread_mutex_lock", status);
-    }
-
-    void unlock()
-    {
-      const int status = pthread_mutex_unlock(&mutex);
-      if (status)
-	error("pthread_mutex_unlock", status);
-    }
-
-    void bcast()
-    {
-      const int status = pthread_cond_broadcast(&cond);
-      if (status)
-	error("pthread_cond_broadcast", status);
-    }
-
-    void cond_wait()
-    {
-      const int status = pthread_cond_wait(&cond, &mutex);
-      if (status)
-	error("pthread_cond_wait", status);
-    }
-
-    bool cond_wait(const unsigned int seconds)
-    {
-      struct timespec ts;
-      if (my_clock_gettime(&ts))
-	error("clock_gettime", errno);
-      ts.tv_sec += seconds;
-      const int status = pthread_cond_timedwait(&cond, &mutex, &ts);
-      if (status == ETIMEDOUT)
-	return true;
-      if (status)
-	error("pthread_cond_timedwait", status);
-      return false;
-    }
-
-    void error(const char *funcname, const int status)
-    {
-      OPENVPN_LOG("PThreadCondBase: " << funcname << " returned " << status);
-      std::abort();
-    }
-
-    pthread_cond_t cond;
-    pthread_mutex_t mutex;
-
-  private:
-    static int my_clock_gettime(struct timespec* ts)
-    {
-#if defined(OPENVPN_PLATFORM_TYPE_APPLE)
-      struct timeval now;
-      const int rv = gettimeofday(&now, NULL);
-      if (rv)
-	return rv;
-      ts->tv_sec  = now.tv_sec;
-      ts->tv_nsec = now.tv_usec * 1000;
-      return 0;
-#elif defined(OPENVPN_PLATFORM_LINUX)
-      return clock_gettime(CLOCK_REALTIME, ts);
-#else
-#error no implementation for my_clock_gettime()
-#endif
-    }
-  };
-
-  // A condition implementation not unlike Windows Events.
-  class PThreadCond : public PThreadCondBase
-  {
-  public:
-    PThreadCond()
-      : signaled(false),
-	signal_counter(0)
-    {
-    }
-
-    // Wait for object to be signaled
-    void wait()
-    {
-      lock();
-      const unsigned int signal_value = signal_counter;
-      while (!signaled && signal_value == signal_counter)
-	cond_wait();
-      unlock();
-    }
-
-    // Wait for object to be signaled,
-    // but return true on timeout
-    bool wait(const unsigned int seconds)
-    {
-      bool ret = false;
-      lock();
-      const unsigned int signal_value = signal_counter;
-      while (!signaled && signal_value == signal_counter && !ret)
-	ret = cond_wait(seconds);
-      unlock();
-      return ret;
-    }
-
-    // Causes wait() to return for all threads blocking on it
-    void signal()
-    {
-      lock();
-      signaled = true;
-      ++signal_counter;
-      bcast();
-      unlock();
-    }
-
-    // Resets the object for re-use
-    void reset()
-    {
-      lock();
-      signaled = false;
-      unlock();
-    }
-
-  private:
-    bool signaled;
-    unsigned int signal_counter;
-  };
 
   // Barrier class that is useful in cases where all threads
   // need to reach a known point before executing some action.
   // Note that this barrier implementation is
   // constructed using pthread conditions.  We don't actually
   // use the native pthread barrier API.
-  class PThreadBarrier : public PThreadCondBase
+  class PThreadBarrier
   {
     enum State {
       UNSIGNALED=0,  // initial state
@@ -190,9 +53,13 @@ namespace openvpn {
     };
 
     PThreadBarrier(const int initial_limit = -1)
-      : state(UNSIGNALED),
-	chosen(false),
-	count(0),
+      : stop(nullptr),
+	limit(initial_limit)
+    {
+    }
+
+    PThreadBarrier(Stop* stop_arg, const int initial_limit = -1)
+      : stop(stop_arg),
 	limit(initial_limit)
     {
     }
@@ -204,15 +71,19 @@ namespace openvpn {
     // signal().
     int wait(const unsigned int seconds)
     {
+      // allow asynchronous stop
+      Stop::Scope stop_scope(stop, [this]() {
+	  error();
+	});
+
       bool timeout = false;
       int ret;
-
-      lock();
+      std::unique_lock<std::mutex> lock(mutex);
       const unsigned int c = ++count;
       while (state == UNSIGNALED
 	     && (limit < 0 || c < limit)
 	     && !timeout)
-	timeout = cond_wait(seconds);
+	timeout = (cv.wait_for(lock, std::chrono::seconds(seconds)) == std::cv_status::timeout);
       if (timeout)
 	ret = TIMEOUT;
       else if (state == ERROR_THROWN)
@@ -224,16 +95,14 @@ namespace openvpn {
 	}
       else
 	ret = SUCCESS;
-      unlock();
       return ret;
     }
 
     void set_limit(const int new_limit)
     {
-      lock();
+      std::unique_lock<std::mutex> lock(mutex);
       limit = new_limit;
-      bcast();
-      unlock();
+      cv.notify_all();
     }
 
     // Generally, only the CHOSEN_ONE calls signal() after its work
@@ -253,18 +122,20 @@ namespace openvpn {
   private:
     void signal_(const State newstate)
     {
-      lock();
+      std::unique_lock<std::mutex> lock(mutex);
       if (state == UNSIGNALED)
 	{
 	  state = newstate;
-	  bcast();
+	  cv.notify_all();
 	}
-      unlock();
     }
 
-    State state;
-    bool chosen;
-    unsigned int count;
+    std::mutex mutex;
+    std::condition_variable cv;
+    Stop* stop;
+    State state{UNSIGNALED};
+    bool chosen = false;
+    unsigned int count = 0;
     int limit;
   };
 
