b614d5c3e18b78621343c2b33a505e3556ea7c29
James Yonan <james@openvpn.net>
2016-06-27 16:39:30 -0600

    random : extended RandomAPI with range methods.

--- openvpn/random/randapi.hpp
+++ openvpn/random/randapi.hpp
@@ -47,11 +47,50 @@ namespace openvpn {
     // Return true on successs, false on fail.
     virtual bool rand_bytes_noexcept(unsigned char *buf, size_t size) = 0;
 
+    // Fill a data object with random bits
     template <typename T>
     void rand_fill(T& obj)
     {
       rand_bytes(reinterpret_cast<unsigned char *>(&obj), sizeof(T));
     }
+
+    // Return a data object with random bits
+    template <typename T>
+    T rand_get()
+    {
+      T ret;
+      rand_fill(ret);
+      return ret;
+    }
+
+    // Return a data object with random bits, always >= 0 for signed types
+    template <typename T>
+    T rand_get_positive()
+    {
+      T ret = rand_get<T>();
+      if (ret < 0)
+	ret = -ret;
+      return ret;
+    }
+
+    // Return a uniformly distributed random number in the range [0, end).
+    // end must be > 0.
+    template <typename T>
+    T randrange(const T end)
+    {
+      return rand_get_positive<T>() % end;
+    }
+
+    // Return a uniformly distributed random number in the range [start, end].
+    template <typename T>
+    T randrange(const T start, const T end)
+    {
+      if (start >= end)
+	return start;
+      else
+	return start + rand_get_positive<T>() % (end - start + 1);
+    }
+
   };
 
 }
