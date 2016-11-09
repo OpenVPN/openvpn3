86480f71f5f052654437a44e7d6e6394b5422c88
James Yonan <james@openvpn.net>
2016-11-09 10:24:58 -0800

    AWS: created mini-SDK for VPC routing API.

---
+++ openvpn/aws/awscreds.hpp
@@ -0,0 +1,33 @@
+//
+//  OpenVPN
+//
+//  Copyright (C) 2012-2016 OpenVPN Technologies, Inc. All rights reserved.
+//
+
+// AWS credentials
+
+#ifndef OPENVPN_AWS_AWSCREDS_H
+#define OPENVPN_AWS_AWSCREDS_H
+
+#include <string>
+
+namespace openvpn {
+  namespace AWS {
+    struct Creds
+    {
+      Creds() {}
+
+      Creds(std::string access_key_arg,
+	    std::string secret_key_arg)
+	: access_key(std::move(access_key_arg)),
+	  secret_key(std::move(secret_key_arg))
+      {
+      }
+
+      std::string access_key;
+      std::string secret_key;
+    };
+  }
+}
+
+#endif
