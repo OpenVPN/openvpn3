1d1632da22e30610523117a03545d32740ac0c5b
James Yonan <james@openvpn.net>
2017-03-11 12:10:18 -0700

    TokenEncrypt : standardize on 128-bit tokens
    Algorithm is considerably simplified (as well as
    having stronger security assurances) by making the
    token size equal to the AES block size.

--- openvpn/openssl/util/tokenencrypt.hpp
+++ openvpn/openssl/util/tokenencrypt.hpp
@@ -9,7 +9,6 @@
 #include <atomic>
 #include <cstdint> // for std::uint8_t
 
-#include <openssl/objects.h>
 #include <openssl/evp.h>
 
 #include <openvpn/common/size.hpp>
@@ -23,17 +22,20 @@ namespace openvpn {
   class TokenEncrypt
   {
   public:
-    static constexpr size_t TOK_SIZE = 32;
-
-    struct Key
+    class Key
     {
+    public:
+      static constexpr size_t SIZE = 16;
+
       Key(RandomAPI& rng)
       {
 	rng.assert_crypto();
 	rng.rand_bytes(data, sizeof(data));
       }
 
-      std::uint8_t data[TOK_SIZE];
+    private:
+      friend class TokenEncrypt;
+      std::uint8_t data[SIZE];
     };
 
     // mode parameter for constructor
@@ -44,7 +46,6 @@ namespace openvpn {
 
     TokenEncrypt(const Key& key, const int mode)
     {
-      static_assert(TOK_SIZE % 16 == 0, "TOK_SIZE must be multiple of AES block size");
       EVP_CIPHER_CTX_init(&ctx);
       if (!EVP_CipherInit_ex(&ctx, EVP_aes_128_ecb(), nullptr, key.data, nullptr, mode))
 	throw OpenSSLException("TokenEncrypt: EVP_CipherInit_ex[1] failed");
@@ -56,34 +57,22 @@ namespace openvpn {
       EVP_CIPHER_CTX_cleanup(&ctx);
     }
 
-    std::string crypt(const std::string& tokstr)
+    // Do the encrypt/decrypt
+    void operator()(std::uint8_t* dest, const std::uint8_t* src, const size_t size)
     {
-      // convert token to binary
-      unsigned char src[TOK_SIZE];
-      Buffer srcbuf(src, TOK_SIZE, false);
-      try {
-	base64->decode(srcbuf, tokstr);
-      }
-      catch (const std::exception& e)
-	{
-	  OPENVPN_THROW_EXCEPTION("TokenEncrypt: base64 decode: " << e.what());
-	}
-      if (srcbuf.size() != TOK_SIZE)
-	OPENVPN_THROW_EXCEPTION("TokenEncrypt: wrong input size, actual=" << srcbuf.size() << " expected=" << TOK_SIZE);
-
-      // crypt it
-      unsigned char dest[TOK_SIZE];
+      // NOTE: since this algorithm uses the ECB block cipher mode,
+      // it should only be used to encrypt/decrypt a message which
+      // is exactly equal to the AES block size (16 bytes).
+      if (size != EVP_CIPHER_CTX_block_size(&ctx))
+	throw Exception("TokenEncrypt: encrypt/decrypt data must be equal to AES block size");
       int outlen=0;
-      if (!EVP_CipherInit_ex (&ctx, nullptr, nullptr, nullptr, nullptr, -1))
+      if (!EVP_CipherInit_ex(&ctx, nullptr, nullptr, nullptr, nullptr, -1))
 	throw OpenSSLException("TokenEncrypt: EVP_CipherInit_ex[2] failed");
-      if (!EVP_CipherUpdate(&ctx, dest, &outlen, src, TOK_SIZE))
+      if (!EVP_CipherUpdate(&ctx, dest, &outlen, src, size))
 	throw OpenSSLException("TokenEncrypt: EVP_CipherUpdate failed");
       // NOTE: we skip EVP_CipherFinal_ex because we are running in ECB mode without padding
-      if (outlen != TOK_SIZE)
-	OPENVPN_THROW_EXCEPTION("TokenEncrypt: unexpected output length=" << outlen);
-
-      // convert result to base64
-      return base64->encode(dest, TOK_SIZE);
+      if (outlen != size)
+	throw Exception("TokenEncrypt: unexpected output length=" + std::to_string(outlen) + " expected=" + std::to_string(size));
     }
 
   private:
