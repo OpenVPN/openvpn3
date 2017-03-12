6e33ef89c0da1122fb9eb7de797df2ce01a7b5f1
James Yonan <james@openvpn.net>
2017-03-12 15:26:57 -0600

    jsonhelper: major refactor
    * Templatize name and title parameters.
    * String construct the json_parse exceptions instead of
      using OPENVPN_THROW.
    * Refactor to_x() methods to derive from get_x() methods
      instead of the other way around.
    * Added get_uint64() method.
    * Added cast_array() method.
    * Added format() method.

--- openvpn/common/jsonhelper.hpp
+++ openvpn/common/jsonhelper.hpp
@@ -22,11 +22,16 @@
 #ifndef OPENVPN_COMMON_JSONHELPER_H
 #define OPENVPN_COMMON_JSONHELPER_H
 
+#include <string>
+#include <cstring>
+#include <cstdint>
+
 #include "json/json.h"
 
 #include <openvpn/common/exception.hpp>
 #include <openvpn/common/string.hpp>
 #include <openvpn/common/file.hpp>
+#include <openvpn/common/stringtempl.hpp>
 #include <openvpn/buffer/bufstr.hpp>
 #include <openvpn/buffer/bufstream.hpp>
 
@@ -36,12 +41,13 @@ namespace openvpn {
   public:
     OPENVPN_EXCEPTION(json_parse);
 
-    static Json::Value parse(const std::string& str, const std::string& title)
+    template <typename TITLE>
+    static Json::Value parse(const std::string& str, const TITLE& title)
     {
       Json::Value root;
       Json::Reader reader;
       if (!reader.parse(str, root, false))
-	OPENVPN_THROW(json_parse, title << " : " << reader.getFormattedErrorMessages());
+	throw json_parse(StringTempl::to_string(title) + " : " + reader.getFormattedErrorMessages());
       return root;
     }
 
@@ -50,17 +56,18 @@ namespace openvpn {
       return parse(read_text_utf8(fn), fn);
     }
 
-    static Json::Value parse_from_buffer(const Buffer& buf, const std::string& title)
+    template <typename TITLE>
+    static Json::Value parse_from_buffer(const Buffer& buf, const TITLE& title)
     {
       Json::Value root;
       Json::Reader reader;
       if (!reader.parse(reinterpret_cast<const char *>(buf.c_data()), reinterpret_cast<const char *>(buf.c_data()) + buf.size(), root, false))
-	OPENVPN_THROW(json_parse, title << " : " << reader.getFormattedErrorMessages());
+	throw json_parse(StringTempl::to_string(title) + " : " + reader.getFormattedErrorMessages());
       return root;
     }
 
-    template <typename T>
-    static void from_vector(Json::Value& root, const T& vec, const std::string& name)
+    template <typename T, typename NAME>
+    static void from_vector(Json::Value& root, const T& vec, const NAME& name)
     {
       Json::Value array(Json::arrayValue);
       for (auto &e : vec)
@@ -69,13 +76,15 @@ namespace openvpn {
 	root[name] = array;
     }
 
-    static void assert_dict(const Json::Value& obj, const std::string& title)
+    template <typename TITLE>
+    static void assert_dict(const Json::Value& obj, const TITLE& title)
     {
       if (!obj.isObject())
-	OPENVPN_THROW(json_parse, title << " is not a JSON dictionary");
+	throw json_parse(StringTempl::to_string(title) + " is not a JSON dictionary");
     }
 
-    static bool is_dict(const Json::Value& obj, const std::string& title)
+    template <typename TITLE>
+    static bool is_dict(const Json::Value& obj, const TITLE& title)
     {
       if (obj.isNull())
 	return false;
@@ -83,94 +92,71 @@ namespace openvpn {
       return true;
     }
 
-    static bool exists(const Json::Value& root, const std::string& name)
+    template <typename NAME>
+    static bool exists(const Json::Value& root, const NAME& name)
     {
       const Json::Value& value = root[name];
       return !value.isNull();
     }
 
-    static void to_string(const Json::Value& root, std::string& dest, const std::string& name, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static void to_string(const Json::Value& root, std::string& dest, const NAME& name, const TITLE& title)
     {
-      const Json::Value& value = root[name];
-      if (value.isNull())
-	OPENVPN_THROW(json_parse, "string " << fmt_name(name, title) << " is missing");
-      if (!value.isString())
-	OPENVPN_THROW(json_parse, "string " << fmt_name(name, title) << " is of incorrect type");
-      dest = value.asString();
+      dest = get_string(root, name, title);
     }
 
+    template <typename NAME, typename TITLE>
     static void to_string_optional(const Json::Value& root,
 				   std::string& dest,
-				   const std::string& name,
+				   const NAME& name,
 				   const std::string& default_value,
-				   const std::string& title)
+				   const TITLE& title)
     {
-      const Json::Value& value = root[name];
-      if (value.isNull())
-	{
-	  dest = default_value;
-	  return;
-	}
-      if (!value.isString())
-	OPENVPN_THROW(json_parse, "string " << fmt_name(name, title) << " is of incorrect type");
-      dest = value.asString();
+      dest = get_string_optional(root, name, default_value, title);
     }
 
-    static void to_int(const Json::Value& root, int& dest, const std::string& name, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static void to_int(const Json::Value& root, int& dest, const NAME& name, const TITLE& title)
     {
-      const Json::Value& value = root[name];
-      if (value.isNull())
-	OPENVPN_THROW(json_parse, "int " << fmt_name(name, title) << " is missing");
-      if (!value.isInt())
-	OPENVPN_THROW(json_parse, "int " << fmt_name(name, title) << " is of incorrect type");
-      dest = value.asInt();
+      dest = get_int(root, name, title);
     }
 
-    static void to_uint(const Json::Value& root, unsigned int& dest, const std::string& name, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static void to_uint(const Json::Value& root, unsigned int& dest, const NAME& name, const TITLE& title)
     {
-      const Json::Value& value = root[name];
-      if (value.isNull())
-	OPENVPN_THROW(json_parse, "uint " << fmt_name(name, title) << " is missing");
-      if (!value.isUInt())
-	OPENVPN_THROW(json_parse, "uint " << fmt_name(name, title) << " is of incorrect type");
-      dest = value.asUInt();
+      dest = get_uint(root, name, title);
     }
 
+    template <typename NAME, typename TITLE>
     static void to_uint_optional(const Json::Value& root,
 				 unsigned int& dest,
-				 const std::string& name,
+				 const NAME& name,
 				 const unsigned int default_value,
-				 const std::string& title)
+				 const TITLE& title)
     {
-      const Json::Value& value = root[name];
-      if (value.isNull())
-	{
-	  dest = default_value;
-	  return;
-	}
-      if (!value.isUInt())
-	OPENVPN_THROW(json_parse, "uint " << fmt_name(name, title) << " is of incorrect type");
-      dest = value.asUInt();
+      dest = get_uint_optional(root, name, default_value, title);
     }
 
-    static void to_bool(const Json::Value& root, bool& dest, const std::string& name, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static void to_uint64(const Json::Value& root, std::uint64_t& dest, const NAME& name, const TITLE& title)
     {
-      const Json::Value& value = root[name];
-      if (value.isNull())
-	OPENVPN_THROW(json_parse, "bool " << fmt_name(name, title) << " is missing");
-      if (!value.isBool())
-	OPENVPN_THROW(json_parse, "bool " << fmt_name(name, title) << " is of incorrect type");
-      dest = value.asBool();
+      dest = get_uint64(root, name, title);
     }
 
-    template <typename T>
-    static void to_vector(const Json::Value& root, T& vec, const std::string& name, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static void to_bool(const Json::Value& root, bool& dest, const NAME& name, const TITLE& title)
+    {
+      dest = get_bool(root, name, title);
+    }
+
+    template <typename T, typename NAME, typename TITLE>
+    static void to_vector(const Json::Value& root, T& vec, const NAME& name, const TITLE& title)
     {
       const Json::Value& array = root[name];
       if (array.isNull())
 	return;
       if (!array.isArray())
-	OPENVPN_THROW(json_parse, "array " << fmt_name(name, title) << " is of incorrect type");
+	throw json_parse("array " + fmt_name(name, title) + " is of incorrect type");
       for (unsigned int i = 0; i < array.size(); ++i)
 	{
 	  vec.emplace_back();
@@ -178,77 +164,118 @@ namespace openvpn {
 	}
     }
 
-    static std::string get_string(const Json::Value& root, const std::string& name, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static std::string get_string(const Json::Value& root, const NAME& name, const TITLE& title)
     {
-      std::string ret;
-      to_string(root, ret, name, title);
-      return ret;
+      const Json::Value& value = root[name];
+      if (value.isNull())
+	throw json_parse("string " + fmt_name(name, title) + " is missing");
+      if (!value.isString())
+	throw json_parse("string " + fmt_name(name, title) + " is of incorrect type");
+      return value.asString();
     }
 
+    template <typename NAME, typename TITLE>
     static std::string get_string_optional(const Json::Value& root,
-					   const std::string& name,
+					   const NAME& name,
 					   const std::string& default_value,
-					   const std::string& title)
+					   const TITLE& title)
     {
-      std::string ret;
-      to_string_optional(root, ret, name, default_value, title);
-      return ret;
+      const Json::Value& value = root[name];
+      if (value.isNull())
+	return default_value;
+      if (!value.isString())
+	throw json_parse("string " + fmt_name(name, title) + " is of incorrect type");
+      return value.asString();
     }
 
-    static std::string get_string(const Json::Value& root, const std::string& name)
+    template <typename NAME>
+    static std::string get_string(const Json::Value& root, const NAME& name)
     {
-      return get_string(root, name, "");
+      return get_string(root, name, nullptr);
     }
 
-    static int get_int(const Json::Value& root, const std::string& name, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static int get_int(const Json::Value& root, const NAME& name, const TITLE& title)
     {
-      int ret;
-      to_int(root, ret, name, title);
-      return ret;
+      const Json::Value& value = root[name];
+      if (value.isNull())
+	throw json_parse("int " + fmt_name(name, title) + " is missing");
+      if (!value.isInt())
+	throw json_parse("int " + fmt_name(name, title) + " is of incorrect type");
+      return value.asInt();
     }
 
-    static int get_int(const Json::Value& root, const std::string& name)
+    template <typename NAME>
+    static int get_int(const Json::Value& root, const NAME& name)
     {
-      return get_int(root, name, "");
+      return get_int(root, name, nullptr);
     }
 
-    static unsigned int get_uint(const Json::Value& root, const std::string& name, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static unsigned int get_uint(const Json::Value& root, const NAME& name, const TITLE& title)
     {
-      unsigned int ret;
-      to_uint(root, ret, name, title);
-      return ret;
+      const Json::Value& value = root[name];
+      if (value.isNull())
+	throw json_parse("uint " + fmt_name(name, title) + " is missing");
+      if (!value.isUInt())
+	throw json_parse("uint " + fmt_name(name, title) + " is of incorrect type");
+      return value.asUInt();
     }
 
-    static unsigned int get_uint(const Json::Value& root, const std::string& name)
+    template <typename NAME>
+    static unsigned int get_uint(const Json::Value& root, const NAME& name)
     {
-      return get_uint(root, name, "");
+      return get_uint(root, name, nullptr);
     }
 
-    static unsigned int get_uint_optional(const Json::Value& root, const std::string& name, const unsigned int default_value, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static unsigned int get_uint_optional(const Json::Value& root, const NAME& name, const unsigned int default_value, const TITLE& title)
     {
-      unsigned int ret;
-      to_uint_optional(root, ret, name, default_value, title);
-      return ret;
+      const Json::Value& value = root[name];
+      if (value.isNull())
+	return default_value;
+      if (!value.isUInt())
+	throw json_parse("uint " + fmt_name(name, title) + " is of incorrect type");
+      return value.asUInt();
     }
 
-    static unsigned int get_uint_optional(const Json::Value& root, const std::string& name, const unsigned int default_value)
+    template <typename NAME>
+    static unsigned int get_uint_optional(const Json::Value& root, const NAME& name, const unsigned int default_value)
     {
-      return get_uint_optional(root, name, default_value, "");
+      return get_uint_optional(root, name, default_value, nullptr);
     }
 
-    static bool get_bool(const Json::Value& root, const std::string& name, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static std::uint64_t get_uint64(const Json::Value& root, const NAME& name, const TITLE& title)
     {
-      bool ret;
-      to_bool(root, ret, name, title);
-      return ret;
+      const Json::Value& value = root[name];
+      if (value.isNull())
+	throw json_parse("uint64 " + fmt_name(name, title) + " is missing");
+      if (!value.isUInt64())
+	throw json_parse("uint64 " + fmt_name(name, title) + " is of incorrect type");
+      return value.asUInt64();
     }
 
-    static bool get_bool(const Json::Value& root, const std::string& name)
+    template <typename NAME, typename TITLE>
+    static bool get_bool(const Json::Value& root, const NAME& name, const TITLE& title)
     {
-      return get_bool(root, name, "");
+      const Json::Value& value = root[name];
+      if (value.isNull())
+	throw json_parse("bool " + fmt_name(name, title) + " is missing");
+      if (!value.isBool())
+	throw json_parse("bool " + fmt_name(name, title) + " is of incorrect type");
+      return value.asBool();
     }
 
-    static bool get_bool_optional(const Json::Value& root, const std::string& name, const bool default_value=false)
+    template <typename NAME>
+    static bool get_bool(const Json::Value& root, const NAME& name)
+    {
+      return get_bool(root, name, nullptr);
+    }
+
+    template <typename NAME>
+    static bool get_bool_optional(const Json::Value& root, const NAME& name, const bool default_value=false)
     {
       const Json::Value& jv = root[name];
       if (jv.isConvertibleTo(Json::booleanValue))
@@ -262,48 +289,66 @@ namespace openvpn {
 	return default_value;
     }
 
-    static const Json::Value& get_dict(const Json::Value& root, const std::string& name, const bool optional, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static const Json::Value& get_dict(const Json::Value& root, const NAME& name, const bool optional, const TITLE& title)
     {
       const Json::Value& value = root[name];
       if (value.isNull())
 	{
 	  if (optional)
 	    return value;
-	  OPENVPN_THROW(json_parse, "dictionary " << fmt_name(name, title) << " is missing");
+	  throw json_parse("dictionary " + fmt_name(name, title) + " is missing");
 	}
       if (!value.isObject())
-	OPENVPN_THROW(json_parse, "dictionary " << fmt_name(name, title) << " is of incorrect type");
+	throw json_parse("dictionary " + fmt_name(name, title) + " is of incorrect type");
       return value;
     }
 
-    static const Json::Value& cast_dict(const Json::Value& value, const bool optional, const std::string& title)
+    template <typename TITLE>
+    static const Json::Value& cast_dict(const Json::Value& value, const bool optional, const TITLE& title)
     {
       if (value.isNull())
 	{
 	  if (optional)
 	    return value;
-	  OPENVPN_THROW(json_parse, "dictionary cast " << title << " is null");
+	  throw json_parse("dictionary cast " + StringTempl::to_string(title) + " is null");
 	}
       if (!value.isObject())
-	OPENVPN_THROW(json_parse, "dictionary cast " << title << " is of incorrect type");
+	throw json_parse("dictionary cast " + StringTempl::to_string(title) + " is of incorrect type");
       return value;
     }
 
-    static const Json::Value& get_array(const Json::Value& root, const std::string& name, const bool optional, const std::string& title)
+    template <typename NAME, typename TITLE>
+    static const Json::Value& get_array(const Json::Value& root, const NAME& name, const bool optional, const TITLE& title)
     {
       const Json::Value& value = root[name];
       if (value.isNull())
 	{
 	  if (optional)
 	    return value;
-	  OPENVPN_THROW(json_parse, "array " << fmt_name(name, title) << " is missing");
+	  throw json_parse("array " + fmt_name(name, title) + " is missing");
 	}
       if (!value.isArray())
-	OPENVPN_THROW(json_parse, "array " << fmt_name(name, title) << " is of incorrect type");
+	throw json_parse("array " + fmt_name(name, title) + " is of incorrect type");
       return value;
     }
 
-    static const Json::Value& get_dict(const Json::Value& root, const std::string& name, const bool optional)
+    template <typename TITLE>
+    static const Json::Value& cast_array(const Json::Value& value, const bool optional, const TITLE& title)
+    {
+      if (value.isNull())
+	{
+	  if (optional)
+	    return value;
+	  throw json_parse("array cast " + StringTempl::to_string(title) + " is null");
+	}
+      if (!value.isArray())
+	throw json_parse("array cast " + StringTempl::to_string(title) + " is of incorrect type");
+      return value;
+    }
+
+    template <typename NAME>
+    static const Json::Value& get_dict(const Json::Value& root, const NAME& name, const bool optional)
     {
       return get_dict(root, name, optional, "");
     }
@@ -324,11 +369,17 @@ namespace openvpn {
       return buf_to_string(*bp);
     }
 
-  private:
-    static std::string fmt_name(const std::string& name, const std::string& title)
+    static std::string format(const Json::Value& root)
     {
-      if (!title.empty())
-	return title + '.' + name;
+      return root.toStyledString();
+    }
+
+  private:
+    template <typename NAME, typename TITLE>
+    static std::string fmt_name(const NAME& name, const TITLE& title)
+    {
+      if (!StringTempl::empty(title))
+	return StringTempl::to_string(title) + '.' + name;
       else
 	return name;
     }
