73f442087817b56a622f8ce3ccbfbe727833837b
James Yonan <james@openvpn.net>
2017-03-12 15:46:28 -0600

    [test/misc] new tests: b64count, cookie, refscope, refscope1, sess_id, sidasm, stat, statatomic, and strtempl.

---
+++ test/misc/refscope1.cpp
@@ -0,0 +1,92 @@
+#include <string>
+#include <iostream>
+#include <utility>
+
+#include <openvpn/log/logsimple.hpp>
+#include <openvpn/common/size.hpp>
+#include <openvpn/common/exception.hpp>
+
+#include <openvpn/random/devurand.hpp>
+#include <openvpn/common/sess_id.hpp>
+#include <openvpn/openssl/util/tokenencrypt.hpp>
+
+using namespace openvpn;
+
+struct SessionID : public SessionID128
+{
+  SessionID()
+  {
+    dump("default");
+  }
+
+  SessionID(RandomAPI& rng)
+    : SessionID128(rng)
+  {
+    dump("rng");
+  }
+
+  ~SessionID()
+  {
+    dump("destruct");
+  }
+
+  void dump(const char *prefix) const
+  {
+    std::cout << prefix << " : " << to_string() << std::endl;
+  }
+};
+
+class Session
+{
+public:
+  Session(RandomAPI& rng)
+    : sid(rng)
+  {
+  }
+
+  const SessionID& get_token() const
+  {
+    return sid;
+  }
+
+private:
+  SessionID sid;
+};
+
+std::string name()
+{
+  return std::string("myname");
+}
+
+void test(Session* session)
+{
+  const std::string& nam = name();
+  const SessionID& sid = session ? session->get_token() : SessionID();
+  std::cout << "Name: " << nam << " SessID: " << sid << std::endl;
+}
+
+void run()
+{
+  DevURand rng;
+  Session sess(rng);
+  std::cout << "--- TEST1 ---" << std::endl;
+  test(&sess);
+  std::cout << "--- TEST2 ---" << std::endl;
+  test(nullptr);
+  std::cout << "--- END ---" << std::endl;
+}
+
+int main(int /*argc*/, char* /*argv*/[])
+{
+  base64_init_static();
+  try {
+    run();
+  }
+  catch (const std::exception& e)
+    {
+      std::cerr << "Exception: " << e.what() << std::endl;
+      return 1;
+    }
+  base64_uninit_static();
+  return 0;
+}
