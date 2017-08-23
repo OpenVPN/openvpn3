db7c5613f3409bb20a59a1495887a0be6185e885
James Yonan <james@openvpn.net>
2017-08-22 22:14:06 -0600

    HTTP layer: added Websocket support
    Signed-off-by: James Yonan <james@openvpn.net>

---
+++ openvpn/common/endian64.hpp
@@ -0,0 +1,22 @@
+//  OpenVPN
+//
+//  Copyright (C) 2012-2017 OpenVPN Technologies, Inc.
+//  All rights reserved.
+
+#pragma once
+
+#include <endian.h>    // __BYTE_ORDER
+#include <cstdint>
+
+namespace openvpn {
+  namespace Endian {
+    std::uint64_t rev64(const std::uint64_t value)
+    {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+      return __bswap_constant_64(value);  // compiler-builtin
+#else
+      return value;
+#endif
+    }
+  }
+}
