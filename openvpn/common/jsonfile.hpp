4110d23d257129787063429187911db1ab0036ce
James Yonan <james@openvpn.net>
2017-08-22 20:43:41 -0600

    Added json::read_fast() and json::write_atomic()
    Signed-off-by: James Yonan <james@openvpn.net>

---
+++ openvpn/common/jsonfile.hpp
@@ -0,0 +1,33 @@
+//  OpenVPN
+//
+//  Copyright (C) 2012-2017 OpenVPN Technologies, Inc.
+//  All rights reserved.
+
+#pragma once
+
+#include <openvpn/common/jsonhelper.hpp>
+#include <openvpn/common/fileatomic.hpp>
+
+namespace openvpn {
+  namespace json {
+
+    inline Json::Value read_fast(const std::string& fn, const bool must_exist)
+    {
+      BufferPtr bp = read_binary_unix(fn, 0, must_exist ? NULL_ON_ENOENT : 0);
+      if (!bp)
+	return Json::Value();
+      return parse_from_buffer(*bp, fn);
+    }
+
+    inline void write_atomic(const std::string& fn,
+			     const std::string& tmpdir,
+			     const mode_t mode,
+			     const Json::Value& root,
+			     const size_t size_hint,
+			     RandomAPI& rng)
+    {
+      BufferPtr bp = new BufferAllocated(size_hint, BufferAllocated::GROW);
+      write_binary_atomic(fn, tmpdir, mode, *bp, rng);
+    }
+  }
+}
