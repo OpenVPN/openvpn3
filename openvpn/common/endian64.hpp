02d95918e4c31fb95d3ae1efac14bf34cb62ea82
James Yonan <james@openvpn.net>
2017-10-24 15:43:02 -0600

    Endian: added missing inline
    Signed-off-by: James Yonan <james@openvpn.net>

--- openvpn/common/endian64.hpp
+++ openvpn/common/endian64.hpp
@@ -10,7 +10,7 @@
 
 namespace openvpn {
   namespace Endian {
-    std::uint64_t rev64(const std::uint64_t value)
+    inline std::uint64_t rev64(const std::uint64_t value)
     {
 #if __BYTE_ORDER == __LITTLE_ENDIAN
       return __bswap_constant_64(value);  // compiler-builtin
