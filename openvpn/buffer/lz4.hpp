8a012b45454b38fa3a7c522a166f88e84b83cbc6
James Yonan <james@openvpn.net>
2018-02-24 11:09:29 -0700

    lz4: added namespace and improved error handling
    * Put all methods in the LZ4 namespace.
    
    * Throw errors instead of returning null BufferPtr().
    
    * For decompress(), make sure that max_decompressed_size
      doesn't exceed LZ4_MAX_INPUT_SIZE.
    
    This commit only affects the standalone LZ4 helper functions,
    not the LZ4 module that is part of the OpenVPN protocol.
    
    Signed-off-by: James Yonan <james@openvpn.net>

--- openvpn/buffer/lz4.hpp
+++ openvpn/buffer/lz4.hpp
@@ -25,62 +25,72 @@
 
 #include <lz4.h>
 
-#include <openvpn/buffer/buffer.hpp>
+#include <openvpn/common/exception.hpp>
 #include <openvpn/common/socktypes.hpp> // for ntohl/htonl
+#include <openvpn/buffer/buffer.hpp>
 
 namespace openvpn {
-  inline BufferPtr compress_lz4(const ConstBuffer& src,
-				const size_t headroom,
-				const size_t tailroom)
-  {
-    // sanity check
-    if (src.size() > LZ4_MAX_INPUT_SIZE)
-      return BufferPtr();
+  namespace LZ4 {
+    OPENVPN_EXCEPTION(lz4_error);
 
-    // allocate dest buffer
-    BufferPtr dest = new BufferAllocated(sizeof(std::uint32_t) + headroom + tailroom + LZ4_COMPRESSBOUND(src.size()), 0);
-    dest->init_headroom(headroom);
-
-    // as a hint to receiver, write the decompressed size
+    inline BufferPtr compress(const ConstBuffer& src,
+			      const size_t headroom,
+			      const size_t tailroom)
     {
-      const std::uint32_t size = htonl(src.size());
-      dest->write(&size, sizeof(size));
+      // sanity check
+      if (src.size() > LZ4_MAX_INPUT_SIZE)
+	OPENVPN_THROW(lz4_error, "compress buffer size=" << src.size() << " exceeds LZ4_MAX_INPUT_SIZE=" << LZ4_MAX_INPUT_SIZE);
+
+      // allocate dest buffer
+      BufferPtr dest = new BufferAllocated(sizeof(std::uint32_t) + headroom + tailroom + LZ4_COMPRESSBOUND(src.size()), 0);
+      dest->init_headroom(headroom);
+
+      // as a hint to receiver, write the decompressed size
+      {
+	const std::uint32_t size = htonl(src.size());
+	dest->write(&size, sizeof(size));
+      }
+
+      // compress
+      const int comp_size = ::LZ4_compress_default((const char *)src.c_data(), (char *)dest->data_end(),
+						   (int)src.size(), (int)dest->remaining(tailroom));
+      if (comp_size <= 0)
+	OPENVPN_THROW(lz4_error, "LZ4_compress_default returned error status=" << comp_size);
+      dest->inc_size(comp_size);
+      return dest;
     }
 
-    // compress
-    const int comp_size = ::LZ4_compress_default((const char *)src.c_data(), (char *)dest->data_end(),
-					       (int)src.size(), (int)dest->remaining(tailroom));
-    if (comp_size <= 0)
-      return BufferPtr();
-    dest->inc_size(comp_size);
-    return dest;
-  }
+    inline BufferPtr decompress(const ConstBuffer& source,
+				const size_t headroom,
+				const size_t tailroom,
+				size_t max_decompressed_size=LZ4_MAX_INPUT_SIZE)
+    {
+      // get the decompressed size
+      ConstBuffer src(source);
+      if (src.size() < sizeof(std::uint32_t))
+	OPENVPN_THROW(lz4_error, "decompress buffer size=" << src.size() << " is too small");
+      std::uint32_t size;
+      src.read(&size, sizeof(size));
+      size = ntohl(size);
+      if (max_decompressed_size > LZ4_MAX_INPUT_SIZE)
+	max_decompressed_size = LZ4_MAX_INPUT_SIZE;
+      if (max_decompressed_size && size > max_decompressed_size)
+	OPENVPN_THROW(lz4_error, "decompress expansion size=" << size << " is too large (must be <= " << max_decompressed_size << ')');
 
-  inline BufferPtr decompress_lz4(const ConstBuffer& source,
-				  const size_t headroom,
-				  const size_t tailroom,
-				  const size_t max_decompressed_size=LZ4_MAX_INPUT_SIZE)
-  {
-    // get the decompressed size
-    ConstBuffer src(source);
-    if (src.size() < sizeof(std::uint32_t))
-      return BufferPtr();
-    std::uint32_t size;
-    src.read(&size, sizeof(size));
-    size = ntohl(size);
-    if (size > max_decompressed_size)
-      return BufferPtr();
+      // allocate dest buffer
+      BufferPtr dest = new BufferAllocated(headroom + tailroom + size, 0);
+      dest->init_headroom(headroom);
 
-    // allocate dest buffer
-    BufferPtr dest = new BufferAllocated(headroom + tailroom + size, 0);
-    dest->init_headroom(headroom);
+      // decompress
+      const int decomp_size = LZ4_decompress_safe((const char *)src.c_data(), (char *)dest->data(),
+						  (int)src.size(), size);
+      if (decomp_size <= 0)
+	OPENVPN_THROW(lz4_error, "LZ4_decompress_safe returned error status=" << decomp_size);
+      if (decomp_size != size)
+	OPENVPN_THROW(lz4_error, "decompress size inconsistency expected_size=" << size << " actual_size=" << decomp_size);
+      dest->inc_size(decomp_size);
+      return dest;
+    }
 
-    // decompress
-    const int decomp_size = LZ4_decompress_safe((const char *)src.c_data(), (char *)dest->data(),
-						(int)src.size(), size);
-    if (decomp_size <= 0 || decomp_size != size)
-      return BufferPtr();
-    dest->inc_size(decomp_size);
-    return dest;
   }
 }
