f05802cf95c71a28c0d6d0e359d3e2e8d0091ad9
James Yonan <james@openvpn.net>
2018-03-07 14:24:38 -0700

    Increase server validation of password size to 16KB to support bundling SAML messages.
    Also allow password to be multi-line UTF-8 text.
    
    Signed-off-by: James Yonan <james@openvpn.net>

--- openvpn/server/servproto.hpp
+++ openvpn/server/servproto.hpp
@@ -327,12 +327,12 @@ namespace openvpn {
 			       const AuthCert::Ptr& auth_cert) override
       {
 	constexpr size_t MAX_USERNAME_SIZE = 256;
-	constexpr size_t MAX_PASSWORD_SIZE = 256;
+	constexpr size_t MAX_PASSWORD_SIZE = 16384;
 
 	if (get_management())
 	  {
 	    AuthCreds::Ptr auth_creds(new AuthCreds(Unicode::utf8_printable(username, MAX_USERNAME_SIZE|Unicode::UTF8_FILTER),
-						    Unicode::utf8_printable(password, MAX_PASSWORD_SIZE|Unicode::UTF8_FILTER),
+						    Unicode::utf8_printable(password, MAX_PASSWORD_SIZE|Unicode::UTF8_FILTER|Unicode::UTF8_PASS_FMT),
 						    Unicode::utf8_printable(peer_info, Unicode::UTF8_FILTER|Unicode::UTF8_PASS_FMT)));
 	    ManLink::send->auth_request(auth_creds, auth_cert, peer_addr);
 	  }
