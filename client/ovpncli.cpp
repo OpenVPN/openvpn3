b7075014a4d70b15463f86c19ab8111a1ba2f6b1
James Yonan <james@openvpn.net>
2018-04-05 17:44:28 -0600

    RemoteOverride: handle exceptions thrown from virtual remote_override() method
    Signed-off-by: James Yonan <james@openvpn.net>

--- client/ovpncli.cpp
+++ client/ovpncli.cpp
@@ -321,20 +321,28 @@ namespace openvpn {
 	  {
 	    const std::string title = "remote-override";
 	    ClientAPI::RemoteOverride ro;
-	    parent->remote_override(ro);
-	    if (!ro.error.empty())
-	      throw Exception("remote override exception: " + ro.error);
+	    try {
+	      parent->remote_override(ro);
+	    }
+	    catch (const std::exception& e)
+	      {
+		ro.error = e.what();
+	      }
 	    RemoteList::Item::Ptr ri(new RemoteList::Item);
-	    if (!ro.ip.empty())
-	      ri->set_ip_addr(IP::Addr(ro.ip, title));
-	    if (ro.host.empty())
-	      ro.host = ro.ip;
-	    HostPort::validate_host(ro.host, title);
-	    HostPort::validate_port(ro.port, title);
-	    ri->server_host = std::move(ro.host);
-	    ri->server_port = std::move(ro.port);
-	    ri->transport_protocol = Protocol::parse(ro.proto, Protocol::CLIENT_SUFFIX, title.c_str());
-
+	    if (ro.error.empty())
+	      {
+		if (!ro.ip.empty())
+		  ri->set_ip_addr(IP::Addr(ro.ip, title));
+		if (ro.host.empty())
+		  ro.host = ro.ip;
+		HostPort::validate_host(ro.host, title);
+		HostPort::validate_port(ro.port, title);
+		ri->server_host = std::move(ro.host);
+		ri->server_port = std::move(ro.port);
+		ri->transport_protocol = Protocol::parse(ro.proto, Protocol::CLIENT_SUFFIX, title.c_str());
+	      }
+	    else
+	      throw Exception("remote override exception: " + ro.error);
 	    return ri;
 	  }
 	else
