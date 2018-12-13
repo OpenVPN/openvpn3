b9b4bb8c557197632aee916e3f2b61984f6135d1
James Yonan <james@openvpn.net>
2018-12-12 22:22:34 -0700

    Endian: use more portable method to determine machine byte order
    Signed-off-by: James Yonan <james@openvpn.net>

--- openvpn/common/endian64.hpp
+++ openvpn/common/endian64.hpp
@@ -5,16 +5,17 @@
 
 #pragma once
 
-#include <endian.h>    // __BYTE_ORDER
 #include <cstdint>
+
+#include <openvpn/common/endian_platform.hpp>
 
 namespace openvpn {
   namespace Endian {
     inline std::uint64_t rev64(const std::uint64_t value)
     {
-#if __BYTE_ORDER == __LITTLE_ENDIAN
+#ifdef OPENVPN_LITTLE_ENDIAN
 #ifdef __clang__
-      return __swap64(value);
+      return __builtin_bswap64(value);
 #else
       return __bswap_constant_64(value);
 #endif
