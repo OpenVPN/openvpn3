868801d7d9474521750e796993c827572e350403
James Yonan <james@openvpn.net>
2019-05-03 08:04:51 -0600

    Linux library: added cpu_time() method to return the CPU time of the current process
    Signed-off-by: James Yonan <james@openvpn.net>

---
+++ openvpn/linux/cputime.hpp
@@ -0,0 +1,56 @@
+//    OpenVPN -- An application to securely tunnel IP networks
+//               over a single port, with support for SSL/TLS-based
+//               session authentication and key exchange,
+//               packet encryption, packet authentication, and
+//               packet compression.
+//
+//    Copyright (C) 2012-2017 OpenVPN Inc.
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License Version 3
+//    as published by the Free Software Foundation.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program in the COPYING file.
+//    If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <string>
+
+#include <openvpn/common/file.hpp>
+#include <openvpn/common/string.hpp>
+#include <openvpn/common/number.hpp>
+#include <openvpn/common/exception.hpp>
+
+namespace openvpn {
+  inline double cpu_time()
+  {
+    try {
+      const std::string stat_fn = "/proc/" + std::to_string(::getpid()) + "/stat";
+      const std::string stat_str = read_text_simple(stat_fn);
+      auto sv = string::split(stat_str, ' ');
+      if (sv.size() < 15)
+	throw Exception(stat_fn + " must have at least 15 fields");
+      const unsigned long utime = parse_number_throw<unsigned long>(sv[13], "error parsing utime");
+      const unsigned long stime = parse_number_throw<unsigned long>(sv[14], "error parsing stime");
+      const long denom = ::sysconf(_SC_CLK_TCK);
+      if (denom < 0)
+	throw Exception("sysconf(_SC_CLK_TCK) failed");
+      return double(utime + stime) / double(denom);
+    }
+    catch (const std::exception& e)
+      {
+	//OPENVPN_LOG("cpu_time exception: " << e.what());
+	return -1.0;
+      }
+  }
+}
