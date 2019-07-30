ff732e3b5dcc200cf1e1ee902739dc4415acaa59
Arne Schwabe <arne@rfc2549.org>
2019-07-30 17:57:30 +0200

    Fix OpenVPN Core build with OpenSSL 1.1.0
    The client hello call back function are only available in 1.1.1+, not
    1.1.0.

--- openvpn/openssl/ssl/sslctx.hpp
+++ openvpn/openssl/ssl/sslctx.hpp
@@ -1158,7 +1158,7 @@ namespace openvpn {
 	      // server-side SNI
 	      if (config->sni_handler)
 		{
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+#if OPENSSL_VERSION_NUMBER >= 0x10101000L
 #define OPENSSL_SERVER_SNI
 		  SSL_CTX_set_client_hello_cb(ctx, client_hello_callback, nullptr);
 #else
@@ -1970,7 +1970,7 @@ namespace openvpn {
       return true;
     }
 
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+#if OPENSSL_VERSION_NUMBER >= 0x10101000L
 
     static int client_hello_callback(::SSL *s, int *al, void *)
     {
