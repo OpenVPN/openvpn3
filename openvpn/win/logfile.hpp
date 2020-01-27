10127e2bdda9f282197e9c6c07972fd7ee9e4a55
Lev Stipakov <lev@openvpn.net>
2020-01-27 11:11:59 +0200

    Add GPL headers
    Signed-off-by: Lev Stipakov <lev@openvpn.net>

--- openvpn/win/logfile.hpp
+++ openvpn/win/logfile.hpp
@@ -1,13 +1,25 @@
+//    OpenVPN -- An application to securely tunnel IP networks
+//               over a single port, with support for SSL/TLS-based
+//               session authentication and key exchange,
+//               packet encryption, packet authentication, and
+//               packet compression.
 //
-//  logfile.hpp
-//  OpenVPN
+//    Copyright (C) 2012-2020 OpenVPN Inc.
 //
-//  Copyright (C) 2012-2017 OpenVPN Technologies, Inc.
-//  All rights reserved.
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License Version 3
+//    as published by the Free Software Foundation.
 //
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program in the COPYING file.
+//    If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef OPENVPN_WIN_LOGFILE_H
-#define OPENVPN_WIN_LOGFILE_H
+#pragma once
 
 #include <openvpn/log/logbase.hpp>
 #include <openvpn/win/logutil.hpp>
@@ -40,5 +52,3 @@ namespace openvpn {
 
   }
 }
-
-#endif
