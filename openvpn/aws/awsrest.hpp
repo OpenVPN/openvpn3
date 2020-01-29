b2e8cb42f05f4b57a0fb341d3ea7911f6634d5ca
Lev Stipakov <lev@openvpn.net>
2020-01-29 15:02:41 +0200

    Add GPL headers
    Signed-off-by: Lev Stipakov <lev@openvpn.net>

--- openvpn/aws/awsrest.hpp
+++ openvpn/aws/awsrest.hpp
@@ -1,13 +1,27 @@
+//    OpenVPN -- An application to securely tunnel IP networks
+//               over a single port, with support for SSL/TLS-based
+//               session authentication and key exchange,
+//               packet encryption, packet authentication, and
+//               packet compression.
 //
-//  OpenVPN
+//    Copyright (C) 2012-2020 OpenVPN Inc.
 //
-//  Copyright (C) 2012-2017 OpenVPN Technologies, Inc. All rights reserved.
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License Version 3
+//    as published by the Free Software Foundation.
 //
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program in the COPYING file.
+//    If not, see <http://www.gnu.org/licenses/>.
 
 // AWS REST API query utilities such as query signing
 
-#ifndef OPENVPN_AWS_AWSREST_H
-#define OPENVPN_AWS_AWSREST_H
+#pragma once
 
 #include <string>
 #include <vector>
@@ -238,5 +252,3 @@ namespace openvpn {
     };
   }
 }
-
-#endif
--- openvpn/aws/awsroute.hpp
+++ openvpn/aws/awsroute.hpp
@@ -1,8 +1,23 @@
+//    OpenVPN -- An application to securely tunnel IP networks
+//               over a single port, with support for SSL/TLS-based
+//               session authentication and key exchange,
+//               packet encryption, packet authentication, and
+//               packet compression.
 //
-//  OpenVPN
+//    Copyright (C) 2012-2020 OpenVPN Inc.
 //
-//  Copyright (C) 2012-2017 OpenVPN Technologies, Inc. All rights reserved.
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License Version 3
+//    as published by the Free Software Foundation.
 //
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program in the COPYING file.
+//    If not, see <http://www.gnu.org/licenses/>.
 
 // Query/Set VPC routes, requires this policy:
 //
@@ -28,8 +43,7 @@
 //     ]
 // }
 
-#ifndef OPENVPN_AWS_AWSROUTE_H
-#define OPENVPN_AWS_AWSROUTE_H
+#pragma once
 
 #include <openvpn/common/xmlhelper.hpp>
 #include <openvpn/aws/awshttp.hpp>
@@ -465,5 +479,3 @@ namespace openvpn {
     };
   }
 }
-
-#endif
