9393206d74eedc2db80d212795df6f500c847f69
Lev Stipakov <lev@openvpn.net>
2020-02-10 16:08:16 +0200

    mingw: add bswap64 implementation for mingw
    Because MinGW doesn't provide it
    (see https://sourceforge.net/p/mingw/mailman/message/18337754/)
    and we use compiler-specific implementations.
    
    Signed-off-by: Lev Stipakov <lev@openvpn.net>

--- openvpn/common/endian64.hpp
+++ openvpn/common/endian64.hpp
@@ -27,6 +27,19 @@
 
 namespace openvpn {
   namespace Endian {
+#ifdef __MINGW32__
+    inline std::uint64_t mingw_bswap64(const std::uint64_t val)
+    {
+      return (((val & (uint64_t)0x00000000000000ffULL) << 56) |
+              ((val & (uint64_t)0x000000000000ff00ULL) << 40) |
+              ((val & (uint64_t)0x0000000000ff0000ULL) << 24) |
+              ((val & (uint64_t)0x00000000ff000000ULL) <<  8) |
+              ((val & (uint64_t)0x000000ff00000000ULL) >>  8) |
+              ((val & (uint64_t)0x0000ff0000000000ULL) >> 24) |
+              ((val & (uint64_t)0x00ff000000000000ULL) >> 40) |
+              ((val & (uint64_t)0xff00000000000000ULL) >> 56));
+    }
+#endif
     inline std::uint64_t rev64(const std::uint64_t value)
     {
 #ifdef OPENVPN_LITTLE_ENDIAN
@@ -34,6 +47,8 @@ namespace openvpn {
       return __builtin_bswap64(value);
 #elif _MSC_VER
       return _byteswap_uint64(value);
+#elif __MINGW32__
+      return mingw_bswap64(value);
 #else
       return __bswap_constant_64(value);
 #endif /* _MSC_VER */
