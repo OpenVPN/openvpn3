97a4b8f9f278f1769f52d44bb580cc13bf925ecf
James Yonan <james@openvpn.net>
2020-06-22 18:00:01 -0600

    HaltRestart: added to_string() method
    Signed-off-by: James Yonan <james@openvpn.net>

--- openvpn/server/servhalt.hpp
+++ openvpn/server/servhalt.hpp
@@ -19,8 +19,9 @@
 //    along with this program in the COPYING file.
 //    If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef OPENVPN_SERVER_SERVHALT_H
-#define OPENVPN_SERVER_SERVHALT_H
+#pragma once
+
+#include <string>
 
 namespace openvpn {
   namespace HaltRestart {
@@ -32,7 +33,26 @@ namespace openvpn {
       AUTH_FAILED,      // auth fail, don't preserve session token
       RAW,              // pass raw message to client
     };
+
+    inline std::string to_string(Type type)
+    {
+      switch (type)
+	{
+	case HALT:
+	  return "HALT";
+	case RESTART:
+	  return "RESTART";
+	case RESTART_PSID:
+	  return "RESTART_PSID";
+	case RESTART_PASSIVE:
+	  return "RESTART_PASSIVE";
+	case AUTH_FAILED:
+	  return "AUTH_FAILED";
+	case RAW:
+	  return "RAW";
+	default:
+	  return "HaltRestart_?";
+	}
+    }
   }
 }
-
-#endif
