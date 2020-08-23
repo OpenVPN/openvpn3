a2bbb1d65cc371ab7e3289bbfe7f11079178f920
James Yonan <james@openvpn.net>
2020-08-23 11:10:42 -0600

    kovpn.hpp: add dependent includes before #include <kovpn/kovpn.h>
    Since userspace and kernel are different build environments,
    it probably makes more sense to move includes in <kovpn/kovpn.h>
    out to their enclosing scopes.
    
    Signed-off-by: James Yonan <james@openvpn.net>

--- openvpn/kovpn/kovpn.hpp
+++ openvpn/kovpn/kovpn.hpp
@@ -21,8 +21,7 @@
 
 // OpenVPN 3 wrapper for kovpn
 
-#ifndef OPENVPN_KOVPN_KOVPN_HPP
-#define OPENVPN_KOVPN_KOVPN_HPP
+#pragma once
 
 // Not including this file causes redefinition errors
 // when the sys/ and linux/ headers below are included
@@ -32,7 +31,7 @@
 #include <sys/socket.h>
 
 extern "C" {
+#include <linux/types.h>
+#include <uapi/ovpn/shim.h> // for OVPN_SHIM_MAX
 #include <kovpn/kovpn.h>
 }
-
-#endif
