038c91e37226368fcb0677d47933a6200521bc8f
Lev Stipakov <lev@openvpn.net>
2021-04-12 11:50:37 +0300

    win: fix non-working DNS resolution after connect
    When server pushes DNS settings to the client, for a minute
    after connection establishment DNS resolution may not work
    in rare cases. This  happens because sometimes Windows picks
    the wrong interface to send DNS requests, and those requests
    are blocked by our WFP rules to prevent DNS leakage.
    
    Clearing the ARP cache after connection fixes the problem.
    
    While on in, switch to more modern API to flush ARP cache.
    
    Signed-off-by: Lev Stipakov <lev@openvpn.net>

--- openvpn/tun/win/tunutil.hpp
+++ openvpn/tun/win/tunutil.hpp
@@ -660,7 +660,7 @@ namespace openvpn {
       inline void flush_arp(const DWORD adapter_index,
 			    std::ostream& os)
       {
-	const DWORD status = ::FlushIpNetTable(adapter_index);
+	const DWORD status = ::FlushIpNetTable2(AF_INET, adapter_index);
 	if (status == NO_ERROR)
 	  os << "TAP: ARP flush succeeded" << std::endl;
 	else
