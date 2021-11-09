8773add86a29ebda091dd343b8f1b4a9450523e3
David Sommerseth <davids@openvpn.net>
2021-11-09 19:28:45 +0100

    endian64: Fix incorrect macro usage
    The #if conditional need to check macros using defined(), otherwise the
    behaviour is not ending up with the expected code.  In most compilers
    these sections will never match.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- openvpn/common/endian64.hpp
+++ openvpn/common/endian64.hpp
@@ -4,7 +4,7 @@
 //               packet encryption, packet authentication, and
 //               packet compression.
 //
-//    Copyright (C) 2012-2020 OpenVPN Inc.
+//    Copyright (C) 2012-2021 OpenVPN Inc.
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU Affero General Public License Version 3
@@ -45,9 +45,9 @@ namespace openvpn {
 #ifdef OPENVPN_LITTLE_ENDIAN
 #ifdef __clang__
       return __builtin_bswap64(value);
-#elif _MSC_VER
+#elif defined(_MSC_VER)
       return _byteswap_uint64(value);
-#elif __MINGW32__
+#elif defined(__MINGW32__)
       return mingw_bswap64(value);
 #else
       return __bswap_constant_64(value);
