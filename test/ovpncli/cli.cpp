94a197493a84778fcb6967fae38e4ba68d06bd50
Frank Lichtenheld <frank@lichtenheld.com>
2022-02-21 13:21:18 +0100

    ovpncli: add missing break in option parsing (CID 11054)
    Introduced in commit
    1b5d913503dfd6d861c32243de55d7f8ece52233
    
    CID 11054 (#1 of 1): Missing break in switch (MISSING_BREAK)
    unterminated_case: The case for value 71 is not terminated
    by a break statement
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- test/ovpncli/cli.cpp
+++ test/ovpncli/cli.cpp
@@ -1046,6 +1046,7 @@ int openvpn_client(int argc, char *argv[], const std::string* profile_content)
 		break;
 	      case 'G':
 		gremlin = optarg;
+		break;
 	      case 'L':
 		enableLegacyAlgorithms = true;
 		break;
