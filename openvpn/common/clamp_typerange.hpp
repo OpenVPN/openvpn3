a9e152522abb3afbc5aa3ec27ae9f8e439a2eb42
Charlie Vigue <charlie.vigue@openvpn.com>
2023-08-21 13:51:50 +0000

    Enhancing clamp_ tooling
    Signed-off-by: Charlie Vigue <charlie.vigue@openvpn.com>

--- openvpn/common/clamp_typerange.hpp
+++ openvpn/common/clamp_typerange.hpp
@@ -24,6 +24,7 @@
 
 #include <limits>
 #include <algorithm>
+#include <functional>
 
 #include "numeric_util.hpp"
 
@@ -65,6 +66,32 @@ OutT clamp_to_typerange(InT inVal)
         auto outMax = static_cast<InT>(std::numeric_limits<OutT>::max());
         return static_cast<OutT>(std::clamp(inVal, outMin, outMax));
     }
+}
+
+/* ============================================================================================================= */
+//  clamp_to_default
+/* ============================================================================================================= */
+
+/**
+ * @brief Adjusts the input value to the default if the input value exceeds the range of the output type
+ *
+ * @tparam OutT Output type
+ * @tparam InT  Input type
+ * @param inVal Input value
+ * @param defVal Input value
+ * @return OutT safely converted from InT, potentially adjusted
+ */
+
+template <typename OutT, typename InT>
+OutT clamp_to_default(InT inVal, OutT defVal)
+{
+    if constexpr (!numeric_util::is_int_rangesafe<OutT, InT>())
+    {
+        if (!is_safe_conversion<OutT>(inVal))
+            return defVal;
+    }
+
+    return static_cast<OutT>(inVal);
 }
 
 } // namespace openvpn::numeric_util
--- openvpn/common/numeric_util.hpp
+++ openvpn/common/numeric_util.hpp
@@ -60,4 +60,48 @@ constexpr bool is_int_rangesafe()
     return is_int_conversion<OutT, InT>() && !is_int_s2u<OutT, InT>() && out_digits >= in_digits;
 }
 
+/* ============================================================================================================= */
+//  is_safe_conversion
+/* ============================================================================================================= */
+
+/**
+ * @brief Returns true if the given value can be contained by the out type
+ *
+ * @tparam OutT Output type
+ * @tparam InT  Input type
+ * @param inVal Input value
+ * @return true | false
+ */
+
+template <typename OutT, typename InT>
+bool is_safe_conversion(InT inVal)
+{
+    if constexpr (!numeric_util::is_int_rangesafe<OutT, InT>())
+    {
+        if constexpr (numeric_util::is_int_u2s<OutT, InT>())
+        {
+            auto unsignedInVal = static_cast<uintmax_t>(inVal);
+            auto outMax = static_cast<uintmax_t>(std::numeric_limits<OutT>::max());
+            if (outMax < unsignedInVal)
+                return false;
+        }
+        else if constexpr (numeric_util::is_int_s2u<OutT, InT>())
+        {
+            auto lowerVal = static_cast<uintmax_t>(std::max(inVal, InT(0)));
+            auto upperLimit = static_cast<uintmax_t>(std::numeric_limits<OutT>::max());
+            if (inVal < 0 || lowerVal > upperLimit)
+                return false;
+        }
+        else
+        {
+            auto outMin = static_cast<InT>(std::numeric_limits<OutT>::min());
+            auto outMax = static_cast<InT>(std::numeric_limits<OutT>::max());
+            if (inVal < outMin || inVal > outMax)
+                return false;
+        }
+    }
+
+    return true;
+}
+
 } // namespace openvpn::numeric_util
