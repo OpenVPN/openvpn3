5494665991eee57b32ea783f784b58d1a327da31
Leonard Ossa <leonard.ossa@openvpn.com>
2024-08-29 18:43:35 +0200

    Refactor PoolType: modernize, add helper methods
    Improve legibility
    
    Signed-off-by: Leonard Ossa <leonard.ossa@openvpn.com>

--- openvpn/addr/pool.hpp
+++ openvpn/addr/pool.hpp
@@ -22,8 +22,9 @@
 #ifndef OPENVPN_ADDR_POOL_H
 #define OPENVPN_ADDR_POOL_H
 
-#include <string>
 #include <deque>
+#include <optional>
+#include <string>
 #include <unordered_map>
 
 #include <openvpn/common/exception.hpp>
@@ -43,23 +44,19 @@ class PoolType
 
     void add_range(const RangeType<ADDR> &range)
     {
-        auto iter = range.iterator();
-        while (iter.more())
+        for (const auto &address : range)
         {
-            const ADDR &a = iter.addr();
-            add_addr(a);
-            iter.next();
+            add_addr(address);
         }
     }
 
     // Add single address to pool (pool will own the address).
     void add_addr(const ADDR &addr)
     {
-        auto e = map.find(addr);
-        if (e == map.end())
+        auto [iter, inserted] = map.try_emplace(addr, false);
+        if (inserted)
         {
             freelist.push_back(addr);
-            map[addr] = false;
         }
     }
 
@@ -97,18 +94,25 @@ class PoolType
         }
     }
 
-    // Acquire a specific address from pool, returning true if
-    // successful, or false if the address is not available.
+    /**
+     * @brief Acquires a specific address from the pool.
+     *
+     * This function attempts to acquire a specific address from the pool. If the address is
+     * available, it marks the address as in use and returns true. If the address is not available,
+     * it returns false.
+     *
+     * @param addr The IP address to acquire.
+     * @return true if the address was successfully acquired, false otherwise.
+     */
     bool acquire_specific_addr(const ADDR &addr)
     {
-        auto e = map.find(addr);
-        if (e != map.end() && !e->second)
+        auto optional_iterator = is_address_available(addr);
+        if (optional_iterator)
         {
-            e->second = true;
+            (*optional_iterator)->second = true;
             return true;
         }
-        else
-            return false;
+        return false;
     }
 
     // Return a previously acquired address to the pool.  Does nothing if
@@ -116,11 +120,11 @@ class PoolType
     // (b) the address is not owned by the pool.
     void release_addr(const ADDR &addr)
     {
-        auto e = map.find(addr);
-        if (e != map.end() && e->second)
+        auto optional_iterator = is_address_in_use(addr);
+        if (optional_iterator)
         {
             freelist.push_back(addr);
-            e->second = false;
+            (*optional_iterator)->second = false;
         }
     }
 
@@ -154,6 +158,31 @@ class PoolType
   private:
     std::deque<ADDR> freelist;
     std::unordered_map<ADDR, bool> map;
+
+    /**
+     * @brief Checks if address is available (free)
+     * @param addr IP Address to check
+     * @return Optional iterator to position; std::nullopt if address is not available
+     */
+    auto is_address_available(const ADDR &addr) -> std::optional<decltype(map.begin())> // Easy decltype for map iterator
+    {
+        auto it = map.find(addr);
+        if (it != map.end() && !it->second)
+            return it;
+        return std::nullopt;
+    }
+
+    /**
+     * @brief Checks if address is in use
+     * @param addr IP Address to check
+     * @return Optional containing iterator to position if found, std::nullopt otherwise
+     */
+    auto is_address_in_use(const ADDR &addr) -> std::optional<decltype(map.begin())> // Easy decltype for map iterator
+    {
+        if (auto it = map.find(addr); it != map.end())
+            return it;
+        return std::nullopt;
+    }
 };
 
 typedef PoolType<IP::Addr> Pool;
