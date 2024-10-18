4c04318e6929382780f7bb2986bdbf82bef9fa8b
Frank Lichtenheld <frank@lichtenheld.com>
2024-10-18 15:17:56 +0200

    UT: Move README-comp.txt content to test_comp.cpp
    No much use in the separate file.
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- test/unittests/test_comp.cpp
+++ test/unittests/test_comp.cpp
@@ -20,6 +20,12 @@
 #define N_COMPRESS 1
 #endif
 
+/**
+   To test and compare speed of the decompression algorithms define N_EXPAND to be
+   more than 1 by either editing this define or by adding a -DN_EXPAND=1000 or
+   -DN_COMPRESS=1000 for the compression speed in the CmakeLists
+   and looks at the time the unit test take for the different combinations.
+*/
 #ifndef N_EXPAND
 #define N_EXPAND 1
 #endif
