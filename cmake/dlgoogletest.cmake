4b9d2c381d9be61f7fffd913933fe9136b8c47d9
Frank Lichtenheld <frank@lichtenheld.com>
2024-11-05 14:14:29 +0000

    CMake: Switch GoogleTest dependency from ExternalProject to FetchContent
    FetchContent is built on top of ExternalProject and makes
    our use-case much easier.
    
    Increase minimum required CMake version to 3.16. 3.14 would probably
    be enough for relevant FetchContent features, but 3.16 is the oldest
    version we actually test (Ubuntu 20.04), so go for that.
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- cmake/dlgoogletest.cmake
+++ cmake/dlgoogletest.cmake
@@ -2,42 +2,19 @@ if (MSVC)
     find_package(GTest CONFIG REQUIRED)
     set(GTEST_LIB GTest::gtest_main)
 else()
+    set(GTEST_LIB gtest_main)
+    if(NOT OVPN_GTEST_VERSION)
+        # renovate: datasource=github-releases depName=google/googletest
+        set(OVPN_GTEST_VERSION v1.15.0)
+    endif()
 
-set(GTEST_LIB gtest_main)
+    include(FetchContent)
 
-# Google Test Unit testing
-# Download and unpack googletest at configure time
-
-# Ensure that this only downloaded and added once
-include_guard(GLOBAL)
-
-if(NOT OVPN_GTEST_VERSION)
-    # renovate: datasource=github-releases depName=google/googletest
-    set(OVPN_GTEST_VERSION v1.15.0)
-endif()
-
-configure_file(${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt.in googletest-download/CMakeLists.txt)
-execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-        RESULT_VARIABLE result
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download )
-if(result)
-    message(FATAL_ERROR "CMake step for googletest failed: ${result}")
-endif()
-execute_process(COMMAND ${CMAKE_COMMAND} --build .
-        RESULT_VARIABLE result
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download )
-if(result)
-    message(FATAL_ERROR "Build step for googletest failed: ${result}")
-endif()
-
-# Prevent overriding the parent project's compiler/linker
-# settings on Windows
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-
-# Add googletest directly to our build. This defines
-# the gtest and gtest_main targets.
-add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/googletest-src
-        ${CMAKE_CURRENT_BINARY_DIR}/googletest-build
-        EXCLUDE_FROM_ALL)
+    FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG        ${OVPN_GTEST_VERSION}
+        )
+    FetchContent_MakeAvailable(googletest)
 
 endif ()
