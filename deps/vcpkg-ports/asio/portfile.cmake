0a75167dd501dbf614326c2215aeb8d6c9785ec5
Frank Lichtenheld <frank@lichtenheld.com>
2025-09-16 13:09:25 +0200

    asio: Move patches to vcpkg-ports directory
    We don't use them anywhere else anymore. And vcpkg
    does not actually check the contents of files outside
    of the port directory for calculating whether it can
    use the cached build.
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- deps/vcpkg-ports/asio/portfile.cmake
+++ deps/vcpkg-ports/asio/portfile.cmake
@@ -8,12 +8,12 @@ vcpkg_from_github(
     SHA512 d44b35d9d1900de35aa10bf339c7e16a06e110377fd70fbefba91599d24cff32cc3dc88a4b0bf1e1706f9ac46177982edb5c7f969b72a57123be6550a3b062d8
     HEAD_REF master
     PATCHES
-        ../../asio/patches/0001-Added-Apple-NAT64-support-when-both-ASIO_HAS_GETADDR.patch
-        ../../asio/patches/0002-Added-user-code-hook-async_connect_post_open-to-be-c.patch
-        ../../asio/patches/0003-error_code.ipp-Use-English-for-Windows-error-message.patch
-        ../../asio/patches/0004-Added-kovpn-route_id-support-to-endpoints-for-sendto.patch
-        ../../asio/patches/0005-basic_resolver_results-added-data-and-cdata-members-.patch
-        ../../asio/patches/0006-reactive_socket_service_base-add-constructor-for-bas.patch
+        0001-Added-Apple-NAT64-support-when-both-ASIO_HAS_GETADDR.patch
+        0002-Added-user-code-hook-async_connect_post_open-to-be-c.patch
+        0003-error_code.ipp-Use-English-for-Windows-error-message.patch
+        0004-Added-kovpn-route_id-support-to-endpoints-for-sendto.patch
+        0005-basic_resolver_results-added-data-and-cdata-members-.patch
+        0006-reactive_socket_service_base-add-constructor-for-bas.patch
 )
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
