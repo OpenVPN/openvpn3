OpenVPN/openvpn3

commit 3e3cd518eadf0b8d9de441f095b841f93dd09fcc

overhaul DNS config tunbuilder API
Remove APIs for setting DNS server addresses, search domains, and adapter
domain suffix and extend DnsOptions to handle DNS related --dhcp-options
as well.

Also add TunProp::Config for controlling whether --dhcp-option search
domains should be stored as split domains. Also try to initialize that
setting (dhcp_search_domains_as_split_domains) per platform based on
existing behavior.

Also provide SWIG binding for class DnsOptions, so that the new API can
be used from Android clients.

Jira: OVPN3-1286
Signed-off-by: Heiko Hund <heiko@openvpn.net>

openvpn/tun/mac/macdns.hpp
@@ -17,6 +17,7 @@
 #include <string>
 #include <sstream>
 #include <memory>
+#include <algorithm>
 
 #include <openvpn/common/size.hpp>
 #include <openvpn/common/exception.hpp>
@@ -43,18 +44,37 @@ class MacDNS : public RC<thread_unsafe_refcount>
       public:
         typedef RCPtr<Config> Ptr;
 
-        Config()
-        {
-        }
+        Config() = default;
 
         Config(const TunBuilderCapture &settings)
-            : have_dns_options(!settings.dns_options.servers.empty()),
-              dns_servers(get_dns_servers(settings)),
-              resolve_domains(get_resolve_domains(settings)),
-              search_domains(get_search_domains(settings)),
-              adapter_domain_suffix(settings.adapter_domain_suffix)
         {
-            if (have_dns_options)
+            for (const auto &[priority, server] : settings.dns_options.servers)
+            {
+                bool dnssec = server.dnssec == DnsServer::Security::Yes;
+                bool secure_transport = server.transport == DnsServer::Transport::HTTPS
+                                        || server.transport == DnsServer::Transport::TLS;
+                bool custom_port = std::any_of(server.addresses.begin(),
+                                               server.addresses.end(),
+                                               [&](const DnsAddress &a)
+                                               { return a.port != 0 && a.port != 53; });
+                if (dnssec || secure_transport || custom_port)
+                {
+                    continue; // unsupported, try next server
+                }
+
+                server_addresses = get_server_addresses(server);
+                resolve_domains = get_resolve_domains(server);
+                break;
+            }
+
+            if (!settings.dns_options.servers.empty() && !CF::array_len(server_addresses))
+            {
+                throw macdns_error("no applicable DNS server config found");
+            }
+
+            search_domains = get_search_domains(settings);
+
+            if (!settings.dns_options.from_dhcp_options)
             {
                 // With --dns options we redirect when there are no split domains
                 redirect_dns = !CF::array_len(resolve_domains);
@@ -64,7 +84,7 @@ class MacDNS : public RC<thread_unsafe_refcount>
                 // We redirect DNS if either of the following is true:
                 // 1. redirect-gateway (IPv4) is pushed, or
                 // 2. DNS servers are pushed but no search domains are pushed
-                redirect_dns = settings.reroute_gw.ipv4 || (CF::array_len(dns_servers) && !CF::array_len(search_domains));
+                redirect_dns = settings.reroute_gw.ipv4 || (CF::array_len(server_addresses) && !CF::array_len(resolve_domains));
             }
         }
 
@@ -73,53 +93,35 @@ class MacDNS : public RC<thread_unsafe_refcount>
             std::ostringstream os;
             os << "RD=" << redirect_dns;
             os << " SO=" << search_order;
-            os << " DNS=" << CF::array_to_string(dns_servers);
+            os << " DNS=" << CF::array_to_string(server_addresses);
             os << " RSD=" << CF::array_to_string(resolve_domains);
             os << " DOM=" << CF::array_to_string(search_domains);
-            os << " ADS=" << adapter_domain_suffix;
             return os.str();
         }
 
-        bool have_dns_options;
         bool redirect_dns = false;
         int search_order = 5000;
-        CF::Array dns_servers;
+        CF::Array server_addresses;
         CF::Array resolve_domains;
         CF::Array search_domains;
-        std::string adapter_domain_suffix;
 
       private:
-        static CF::Array get_dns_servers(const TunBuilderCapture &settings)
+        static CF::Array get_server_addresses(const DnsServer &server)
         {
             CF::MutableArray ret(CF::mutable_array());
-            if (!settings.dns_options.servers.empty())
+            for (const auto &ip : server.addresses)
             {
-                const auto &server = settings.dns_options.servers.begin()->second;
-                for (const auto &ip : server.addresses)
-                {
-                    CF::array_append_str(ret, ip.address.to_string());
-                }
-            }
-            else
-            {
-                for (const auto &server : settings.dns_servers)
-                {
-                    CF::array_append_str(ret, server.address);
-                }
+                CF::array_append_str(ret, ip.address);
             }
             return CF::const_array(ret);
         }
 
-        static CF::Array get_resolve_domains(const TunBuilderCapture &settings)
+        static CF::Array get_resolve_domains(const DnsServer &server)
         {
             CF::MutableArray ret(CF::mutable_array());
-            if (!settings.dns_options.servers.empty())
+            for (const auto &rd : server.domains)
             {
-                const auto &server = settings.dns_options.servers.begin()->second;
-                for (const auto &rd : server.domains)
-                {
-                    CF::array_append_str(ret, rd.domain);
-                }
+                CF::array_append_str(ret, rd.domain);
             }
             return CF::const_array(ret);
         }
@@ -130,13 +132,6 @@ class MacDNS : public RC<thread_unsafe_refcount>
             if (!settings.dns_options.servers.empty())
             {
                 for (const auto &sd : settings.dns_options.search_domains)
-                {
-                    CF::array_append_str(ret, sd.domain);
-                }
-            }
-            else
-            {
-                for (const auto &sd : settings.search_domains)
                 {
                     CF::array_append_str(ret, sd.domain);
                 }
@@ -197,31 +192,17 @@ class MacDNS : public RC<thread_unsafe_refcount>
                 info->dns.will_modify();
 
                 // set DNS servers
-                if (CF::array_len(config.dns_servers))
+                if (CF::array_len(config.server_addresses))
                 {
                     info->dns.backup_orig("ServerAddresses");
-                    CF::dict_set_obj(info->dns.mod, "ServerAddresses", config.dns_servers());
+                    CF::dict_set_obj(info->dns.mod, "ServerAddresses", config.server_addresses());
                 }
 
                 // set search domains
                 info->dns.backup_orig("SearchDomains");
 
-                if (config.have_dns_options)
-                {
-                    if (CF::array_len(config.search_domains))
-                        CF::dict_set_obj(info->dns.mod, "SearchDomains", config.search_domains());
-                }
-                else
-                {
-                    // add adapter_domain_suffix to SearchDomains for domain autocompletion
-                    CF::MutableArray search_domains(CF::mutable_array());
-
-                    if (config.adapter_domain_suffix.length() > 0)
-                        CF::array_append_str(search_domains, config.adapter_domain_suffix);
-
-                    if (CF::array_len(search_domains))
-                        CF::dict_set_obj(info->dns.mod, "SearchDomains", search_domains());
-                }
+                if (CF::array_len(config.search_domains))
+                    CF::dict_set_obj(info->dns.mod, "SearchDomains", config.search_domains());
 
                 // set search order
                 info->dns.backup_orig("SearchOrder");
@@ -234,38 +215,22 @@ class MacDNS : public RC<thread_unsafe_refcount>
             {
                 // split-DNS - resolve only specific domains
                 info->ovpn.mod_reset();
-                if (config.have_dns_options)
+
+                // set DNS servers
+                CF::dict_set_obj(info->ovpn.mod, "ServerAddresses", config.server_addresses());
+
+                // DNS will be used only for those domains
+                CF::dict_set_obj(info->ovpn.mod, "SupplementalMatchDomains", config.resolve_domains());
+
+                // do not use those domains in autocompletion
+                CF::dict_set_int(info->ovpn.mod, "SupplementalMatchDomainsNoSearch", 1);
+
+                // set search domains if there are any
+                if (CF::array_len(config.search_domains))
                 {
-                    // set DNS servers
-                    CF::dict_set_obj(info->ovpn.mod, "ServerAddresses", config.dns_servers());
-
-                    // DNS will be used only for those domains
-                    CF::dict_set_obj(info->ovpn.mod, "SupplementalMatchDomains", config.resolve_domains());
-
-                    // do not use those domains in autocompletion
-                    CF::dict_set_int(info->ovpn.mod, "SupplementalMatchDomainsNoSearch", 1);
-
-                    // set search domains if there are any
-                    if (CF::array_len(config.search_domains))
-                    {
-                        info->dns.backup_orig("SearchDomains");
-                        CF::dict_set_obj(info->dns.mod, "SearchDomains", config.search_domains());
-                        mod |= info->dns.push_to_store();
-                    }
-                }
-                else
-                {
-                    if (CF::array_len(config.dns_servers) && CF::array_len(config.search_domains))
-                    {
-                        // set DNS servers
-                        CF::dict_set_obj(info->ovpn.mod, "ServerAddresses", config.dns_servers());
-
-                        // DNS will be used only for those domains
-                        CF::dict_set_obj(info->ovpn.mod, "SupplementalMatchDomains", config.search_domains());
-
-                        // do not use those domains in autocompletion
-                        CF::dict_set_int(info->ovpn.mod, "SupplementalMatchDomainsNoSearch", 1);
-                    }
+                    info->dns.backup_orig("SearchDomains");
+                    CF::dict_set_obj(info->dns.mod, "SearchDomains", config.search_domains());
+                    mod |= info->dns.push_to_store();
                 }
 
                 // in case of split-DNS macOS uses domain suffix of network adapter,

client/ovpncli.hpp
@@ -209,6 +209,14 @@ struct ConfigCommon
     // DNS servers, use the standard Google DNS servers.
     bool googleDnsFallback = false;
 
+    // If true --dhcp-option DOMAIN{-SEARCH} are parsed as split
+    // domains, ADAPTER_DOMAIN_SUFFIX is the only search domain
+#if defined(OPENVPN_PLATFORM_WIN) || defined(OPENVPN_PLATFORM_MAC) || defined(OPENVPN_PLATFORM_LINUX) || defined(OPENVPN_PLATFORM_IPHONE)
+    bool dhcpSearchDomainsAsSplitDomains = true;
+#else
+    bool dhcpSearchDomainsAsSplitDomains = false;
+#endif
+
     // if true, do synchronous DNS lookup.
     bool synchronousDnsLookup = false;
 

client/ovpncli.i
@@ -6,6 +6,7 @@
 
 %include "std_string.i" // for std::string typemaps
 %include "std_vector.i"
+%include "std_map.i"
 
 // top-level C++ implementation file
 %{
@@ -54,8 +55,12 @@ namespace std {
   %template(ClientAPI_LLVector) vector<long long>;
   %template(ClientAPI_StringVec) vector<string>;
 };
+%template(DnsOptions_AddressList) std::vector<openvpn::DnsAddress>;
+%template(DnsOptions_DomainsList) std::vector<openvpn::DnsDomain>;
+%template(DnsOptions_ServersMap) std::map<int, openvpn::DnsServer>;
 
 // interface to be bridged between C++ and target language
+%include "openvpn/client/dns_options.hpp"
 %include "openvpn/pki/epkibase.hpp"
 %include "openvpn/tun/builder/base.hpp"
 %import  "openvpn/tun/extern/fw.hpp"     // ignored

openvpn/client/cliopt.hpp
@@ -356,6 +356,7 @@ class ClientOptions : public RC<thread_unsafe_refcount>
                 tunconf.tun_prop.mtu = tun_mtu;
             tunconf.tun_prop.mtu_max = tun_mtu_max;
             tunconf.tun_prop.google_dns_fallback = config.clientconf.googleDnsFallback;
+            tunconf.tun_prop.dhcp_search_domains_as_split_domains = config.clientconf.dhcpSearchDomainsAsSplitDomains;
             tunconf.tun_prop.remote_list = remote_list;
             tunconf.stop = config.stop;
             tunconf.allow_local_dns_resolvers = config.clientconf.allowLocalDnsResolvers;
@@ -373,6 +374,7 @@ class ClientOptions : public RC<thread_unsafe_refcount>
                 tunconf.tun_prop.layer = layer;
                 tunconf.tun_prop.session_name = session_name;
                 tunconf.tun_prop.google_dns_fallback = config.clientconf.googleDnsFallback;
+                tunconf.tun_prop.dhcp_search_domains_as_split_domains = config.clientconf.dhcpSearchDomainsAsSplitDomains;
                 if (tun_mtu)
                     tunconf.tun_prop.mtu = tun_mtu;
                 tunconf.tun_prop.mtu_max = tun_mtu_max;
@@ -391,6 +393,7 @@ class ClientOptions : public RC<thread_unsafe_refcount>
                 tunconf->builder = config.builder;
                 tunconf->tun_prop.session_name = session_name;
                 tunconf->tun_prop.google_dns_fallback = config.clientconf.googleDnsFallback;
+                tunconf->tun_prop.dhcp_search_domains_as_split_domains = config.clientconf.dhcpSearchDomainsAsSplitDomains;
                 tunconf->tun_prop.allow_local_lan_access = config.clientconf.allowLocalLanAccess;
                 if (tun_mtu)
                     tunconf->tun_prop.mtu = tun_mtu;
@@ -433,6 +436,7 @@ class ClientOptions : public RC<thread_unsafe_refcount>
                     tunconf->tun_prop.mtu = tun_mtu;
                 tunconf->tun_prop.mtu_max = tun_mtu_max;
                 tunconf->tun_prop.google_dns_fallback = config.clientconf.googleDnsFallback;
+                tunconf->tun_prop.dhcp_search_domains_as_split_domains = config.clientconf.dhcpSearchDomainsAsSplitDomains;
                 tunconf->generate_tun_builder_capture_event = config.clientconf.generateTunBuilderCaptureEvent;
                 tunconf->tun_prop.remote_list = remote_list;
                 tunconf->frame = frame;
@@ -448,6 +452,7 @@ class ClientOptions : public RC<thread_unsafe_refcount>
                 tunconf->tun_prop.layer = layer;
                 tunconf->tun_prop.session_name = session_name;
                 tunconf->tun_prop.google_dns_fallback = config.clientconf.googleDnsFallback;
+                tunconf->tun_prop.dhcp_search_domains_as_split_domains = config.clientconf.dhcpSearchDomainsAsSplitDomains;
                 if (tun_mtu)
                     tunconf->tun_prop.mtu = tun_mtu;
                 tunconf->tun_prop.mtu_max = tun_mtu_max;
@@ -475,6 +480,7 @@ class ClientOptions : public RC<thread_unsafe_refcount>
                 tunconf->tun_prop.layer = layer;
                 tunconf->tun_prop.session_name = session_name;
                 tunconf->tun_prop.google_dns_fallback = config.clientconf.googleDnsFallback;
+                tunconf->tun_prop.dhcp_search_domains_as_split_domains = config.clientconf.dhcpSearchDomainsAsSplitDomains;
                 if (tun_mtu)
                     tunconf->tun_prop.mtu = tun_mtu;
                 tunconf->tun_prop.mtu_max = tun_mtu_max;

openvpn/client/dns_options.hpp
@@ -1,0 +1,362 @@
+//    OpenVPN -- An application to securely tunnel IP networks
+//               over a single port, with support for SSL/TLS-based
+//               session authentication and key exchange,
+//               packet encryption, packet authentication, and
+//               packet compression.
+//
+//    Copyright (C) 2022- OpenVPN Inc.
+//
+//    SPDX-License-Identifier: MPL-2.0 OR AGPL-3.0-only WITH openvpn3-openssl-exception
+//
+
+
+#pragma once
+
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+#include <openvpn/common/number.hpp>
+#include <openvpn/common/jsonlib.hpp>
+#include <openvpn/common/hostport.hpp>
+#include <openvpn/addr/ip.hpp>
+
+#ifdef HAVE_JSON
+#include <openvpn/common/jsonhelper.hpp>
+#endif
+
+namespace openvpn {
+
+/**
+ * @class DnsAddress
+ * @brief A name server address and optional port
+ */
+struct DnsAddress
+{
+    /**
+     * @brief Return string representation of the DnsAddress object
+     *
+     * @return std::string  the string representation generated
+     */
+    std::string to_string() const
+    {
+        std::ostringstream os;
+        os << address;
+        if (port)
+        {
+            os << " " << port;
+        }
+        return os.str();
+    }
+
+    void validate(const std::string &title) const
+    {
+        IP::Addr::validate(address, title);
+    }
+
+#ifdef HAVE_JSON
+    Json::Value to_json() const
+    {
+        Json::Value root(Json::objectValue);
+        root["address"] = Json::Value(address);
+        if (port)
+        {
+            root["port"] = Json::Value(port);
+        }
+        return root;
+    }
+
+    void from_json(const Json::Value &root, const std::string &title)
+    {
+        json::assert_dict(root, title);
+        json::to_uint_optional(root, port, "port", 0u, title);
+
+        std::string addr_str;
+        json::to_string(root, addr_str, "address", title);
+        address = IP::Addr::from_string(addr_str).to_string();
+    }
+#endif
+
+    std::string address;
+    unsigned int port = 0;
+};
+
+/**
+ * @class DnsDomain
+ * @brief A DNS domain name
+ */
+struct DnsDomain
+{
+    /**
+     * @brief Return string representation of the DnsDomain object
+     *
+     * @return std::string  the string representation generated
+     */
+    std::string to_string() const
+    {
+        return domain;
+    }
+
+    void validate(const std::string &title) const
+    {
+        HostPort::validate_host(domain, title);
+    }
+
+#ifdef HAVE_JSON
+    Json::Value to_json() const
+    {
+        return Json::Value(domain);
+    }
+
+    void from_json(const Json::Value &value, const std::string &title)
+    {
+        if (!value.isString())
+        {
+            throw json::json_parse("string " + title + " is of incorrect type");
+        }
+        domain = value.asString();
+    }
+#endif
+
+    std::string domain;
+};
+
+/**
+ * @class DnsServer
+ * @brief DNS settings for a name server
+ */
+struct DnsServer
+{
+    enum class Security
+    {
+        Unset,
+        No,
+        Yes,
+        Optional
+    };
+
+    std::string dnssec_string(const Security dnssec) const
+    {
+        switch (dnssec)
+        {
+        case Security::No:
+            return "No";
+        case Security::Yes:
+            return "Yes";
+        case Security::Optional:
+            return "Optional";
+        default:
+            return "Unset";
+        }
+    }
+
+    enum class Transport
+    {
+        Unset,
+        Plain,
+        HTTPS,
+        TLS
+    };
+
+    std::string transport_string(const Transport transport) const
+    {
+        switch (transport)
+        {
+        case Transport::Plain:
+            return "Plain";
+        case Transport::HTTPS:
+            return "HTTPS";
+        case Transport::TLS:
+            return "TLS";
+        default:
+            return "Unset";
+        }
+    }
+
+    std::string to_string(const char *prefix = "") const
+    {
+        std::ostringstream os;
+        os << prefix << "Addresses:\n";
+        for (const auto &address : addresses)
+        {
+            os << prefix << "  " << address.to_string() << '\n';
+        }
+        if (!domains.empty())
+        {
+            os << prefix << "Domains:\n";
+            for (const auto &domain : domains)
+            {
+                os << prefix << "  " << domain.to_string() << '\n';
+            }
+        }
+        if (dnssec != Security::Unset)
+        {
+            os << prefix << "DNSSEC: " << dnssec_string(dnssec) << '\n';
+        }
+        if (transport != Transport::Unset)
+        {
+            os << prefix << "Transport: " << transport_string(transport) << '\n';
+        }
+        if (!sni.empty())
+        {
+            os << prefix << "SNI: " << sni << '\n';
+        }
+        return os.str();
+    }
+
+#ifdef HAVE_JSON
+    Json::Value to_json() const
+    {
+        Json::Value server(Json::objectValue);
+        json::from_vector(server, addresses, "addresses");
+        if (!domains.empty())
+        {
+            json::from_vector(server, domains, "domains");
+        }
+        if (dnssec != Security::Unset)
+        {
+            server["dnssec"] = Json::Value(dnssec_string(dnssec));
+        }
+        if (transport != Transport::Unset)
+        {
+            server["transport"] = Json::Value(transport_string(transport));
+        }
+        if (!sni.empty())
+        {
+            server["sni"] = Json::Value(sni);
+        }
+        return server;
+    }
+
+    void from_json(const Json::Value &root, const std::string &title)
+    {
+        json::assert_dict(root, title);
+        json::to_vector(root, addresses, "addresses", title);
+        if (json::exists(root, "domains"))
+        {
+            json::to_vector(root, domains, "domains", title);
+        }
+        if (json::exists(root, "dnssec"))
+        {
+            std::string dnssec_str;
+            json::to_string(root, dnssec_str, "dnssec", title);
+            if (dnssec_str == "Optional")
+            {
+                dnssec = Security::Optional;
+            }
+            else if (dnssec_str == "Yes")
+            {
+                dnssec = Security::Yes;
+            }
+            else if (dnssec_str == "No")
+            {
+                dnssec = Security::No;
+            }
+            else
+            {
+                throw json::json_parse("dnssec value " + dnssec_str + "is unknown");
+            }
+        }
+        if (json::exists(root, "transport"))
+        {
+            std::string transport_str;
+            json::to_string(root, transport_str, "transport", title);
+            if (transport_str == "Plain")
+            {
+                transport = Transport::Plain;
+            }
+            else if (transport_str == "HTTPS")
+            {
+                transport = Transport::HTTPS;
+            }
+            else if (transport_str == "TLS")
+            {
+                transport = Transport::TLS;
+            }
+            else
+            {
+                throw json::json_parse("transport value " + transport_str + "is unknown");
+            }
+        }
+        json::to_string_optional(root, sni, "sni", "", title);
+    }
+#endif
+
+    std::vector<DnsAddress> addresses;
+    std::vector<DnsDomain> domains;
+    Security dnssec = Security::Unset;
+    Transport transport = Transport::Unset;
+    std::string sni;
+};
+
+/**
+ * @class DnsOptions
+ * @brief All DNS options set with the --dns or --dhcp-option directive
+ */
+struct DnsOptions
+{
+    std::string to_string() const
+    {
+        std::ostringstream os;
+        if (!servers.empty())
+        {
+            os << "DNS Servers:\n";
+            for (const auto &elem : servers)
+            {
+                os << "  Priority: " << elem.first << '\n';
+                os << elem.second.to_string("  ");
+            }
+        }
+        if (!search_domains.empty())
+        {
+            os << "DNS Search Domains:\n";
+            for (const auto &domain : search_domains)
+            {
+                os << "  " << domain.to_string() << '\n';
+            }
+        }
+        return os.str();
+    }
+
+#ifdef HAVE_JSON
+    Json::Value to_json() const
+    {
+        Json::Value root(Json::objectValue);
+        Json::Value servers_json(Json::objectValue);
+        for (const auto &[prio, server] : servers)
+        {
+            servers_json[std::to_string(prio)] = server.to_json();
+        }
+        root["servers"] = std::move(servers_json);
+        json::from_vector(root, search_domains, "search_domains");
+        return root;
+    }
+
+    void from_json(const Json::Value &root, const std::string &title)
+    {
+        json::assert_dict(root, title);
+        json::assert_dict(root["servers"], title);
+        for (const auto &prio : root["servers"].getMemberNames())
+        {
+            DnsServer server;
+            server.from_json(root["servers"][prio], title);
+            servers[std::stoi(prio)] = std::move(server);
+        }
+        json::to_vector(root, search_domains, "search_domains", title);
+    }
+#endif
+
+    bool from_dhcp_options = false;
+    std::vector<DnsDomain> search_domains;
+    std::map<int, DnsServer> servers;
+
+  protected:
+    DnsServer &get_server(const int priority)
+    {
+        auto it = servers.insert(std::make_pair(priority, DnsServer())).first;
+        return (*it).second;
+    }
+};
+
+} // namespace openvpn

openvpn/client/optfilt.hpp
@@ -95,7 +95,7 @@ class PushedOptionsFilter : public OptionList::FilterBase
         if (o.size() >= 3
             && o.ref(0) == "dns"
             && o.ref(1) == "server"
-            && DnsServer::parse_priority(o.ref(2)) < 0)
+            && DnsOptionsParser::parse_priority(o.ref(2)) < 0)
         {
             throw option_error(ERR_INVALID_CONFIG, o.escape(false));
         }

openvpn/common/hostport.hpp
@@ -16,7 +16,6 @@
 
 #include <openvpn/common/exception.hpp>
 #include <openvpn/common/number.hpp>
-#include <openvpn/common/options.hpp>
 #include <openvpn/common/unicode.hpp>
 
 namespace openvpn::HostPort {

commit 0fe0db35a9d4f21daf9d43b926c2da2f4cc882d2

macdns: redirect DNS only when servers available
The changes in 3e3cd518ea caused the redirect DNS flag to be set, even
though there are no DNS servers pushed. This causes a regression, where
pre-existing search domains get deleted on disconnect. Fix is to restore
the previous behavior: redirect only when we have at least one server.

Jira: OVPN3-1423
Signed-off-by: Heiko Hund <heiko@openvpn.net>

openvpn/tun/mac/macdns.hpp
@@ -74,17 +74,20 @@ class MacDNS : public RC<thread_unsafe_refcount>
 
             search_domains = get_search_domains(settings);
 
-            if (!settings.dns_options.from_dhcp_options)
+            if (CF::array_len(server_addresses))
             {
-                // With --dns options we redirect when there are no split domains
-                redirect_dns = !CF::array_len(resolve_domains);
-            }
-            else
-            {
-                // We redirect DNS if either of the following is true:
-                // 1. redirect-gateway (IPv4) is pushed, or
-                // 2. DNS servers are pushed but no search domains are pushed
-                redirect_dns = settings.reroute_gw.ipv4 || (CF::array_len(server_addresses) && !CF::array_len(resolve_domains));
+                if (!settings.dns_options.from_dhcp_options)
+                {
+                    // With --dns options we redirect when there are no split domains
+                    redirect_dns = !CF::array_len(resolve_domains);
+                }
+                else
+                {
+                    // We redirect DNS if either of the following is true:
+                    // 1. redirect-gateway (IPv4) is pushed, or
+                    // 2. DNS servers are pushed but no search domains are pushed
+                    redirect_dns = settings.reroute_gw.ipv4 || !CF::array_len(resolve_domains);
+                }
             }
         }
 